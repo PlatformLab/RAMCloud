c863c35179abc1a12160229c53d2339e4a30656a
Ryan Stutsman <stutsman@cs.stanford.edu>
2009-11-18 21:14:43 -0800

    Inline C++ Net wrapper

--- src/server/net.h
+++ src/server/net.h
@@ -16,14 +16,20 @@ class Net {
 
 class UDPNet : public Net {
   public:
-    virtual void Connect();
-    virtual int Close();
-    virtual int IsServer();
-    virtual int IsConnected();
-    virtual int SendRPC(struct rcrpc *);
-    virtual int RecvRPC(struct rcrpc **);
-    virtual ~UDPNet();
-    UDPNet(bool is_server);
+    UDPNet(bool is_server) : net(rc_udp_net()) {
+        rc_udp_net_init(&net, is_server);
+    }
+    virtual void Connect() { net.net.connect((rc_net*)&net); }
+    virtual int Close() { return net.net.close((rc_net*)&net); }
+    virtual int IsServer() { return net.net.is_server((rc_net*)&net); }
+    virtual int IsConnected() { return net.net.is_connected((rc_net*)&net); }
+    virtual int SendRPC(struct rcrpc *msg) {
+        return net.net.send_rpc((rc_net*)&net, msg);
+    }
+    virtual int RecvRPC(struct rcrpc **msg) {
+        return net.net.recv_rpc((rc_net*)&net, msg);
+    }
+    virtual ~UDPNet() {}
   private:
     rc_udp_net net;
 };
