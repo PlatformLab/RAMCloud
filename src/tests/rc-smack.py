aad00def0375c170f65303a1efeaca1221a2ff6a
Stephen M. Rumble <rumble@cs.stanford.edu>
2009-12-10 18:10:40 -0800

    Fix another few tricky log bugs.
    This is becoming a quagmire. The problem is that appending to the log may
    cause objects to move around when you don't expect it, since the cleaner
    has been triggered in the meantime in order to satisfy the write.
    
    For example, the following is dangerous:
        old_obj = hash->lookup(key)
        log->append(new_obj)
        hash->put(key, new_obj)
        log->free(old_obj)
    
    Why? Because old_obj may have been relocated when appending new_obj to
    the log. The pointer is invalidated. In general, when appending to the
    log, any pointers you have into the log may become invalidated.
    
    Pretty sweet, huh? Time to rethink this...

--- src/tests/rc-smack.py
+++ src/tests/rc-smack.py
@@ -1,13 +1,19 @@
 import ramcloud, random
 
+# get a buffer of random printable characters
+def getrandbuf(len):
+	i = 0
+	randbuf = ""
+	while i < len:
+		randbuf += chr(random.randint(32, 126))
+		i += 1
+	return randbuf
+
+
 # re-write random data to the same key over and over,
 # making sure to re-read each time and confirm equality
 def rewrite_smack(c):
-	i = 0
-	randbuf = ""
-	while i < 2000:
-		randbuf += chr(random.randint(32, 126))
-		i += 1
+	randbuf = getrandbuf(2000)
 
 	i = 0
 	while i < 1000000:
@@ -19,6 +25,54 @@ def rewrite_smack(c):
 			break
 		i += 1
 
+# insert a value into the table, confirm re-read, and
+# immediately delete it over and over. should exercise
+# the tombstone code well
+def delete_smack(c):
+	randbuf = getrandbuf(2000)
+
+	i = 0
+	while i < 1000000:
+		buf = randbuf[random.randint(0, 2000):]
+		buf = buf[0:1000]
+		key = c.insert(0, buf)
+		if c.read(0, key) != buf:
+			print "HOSED"
+			break
+		c.delete(0, key)
+		i += 1
+
+# write a value, then either re-write a new one over it, or delete
+# first and then write out the new one (0.5 probability of each case)
+def rewrite_delete_smack(c):
+	randbuf = getrandbuf(2000)
+
+	c.write(0, 0, randbuf[0:1000])
+
+	i = 0
+	while i < 10000000:
+		buf = randbuf[random.randint(0, 2000):]
+		buf = buf[0:1000]
+		
+		if random.random() < 0.5:
+			c.write(0, 0, buf)
+		else:
+			c.delete(0, 0)
+			c.write(0, 0, buf)
+
+		if c.read(0, 0) != buf:
+			print "HOSED"
+			break 
+
+
 c = ramcloud.RAMCloud()
 c.connect()
+
+print "Running rewrite smack"
 rewrite_smack(c)
+
+print "Running delete smack"
+delete_smack(c)
+
+print "Running random rewrite/delete smack"
+rewrite_delete_smack(c)
