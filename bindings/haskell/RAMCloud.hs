dc5cd4d58f6f0cde3ece097abe4bc8981a299bad
Ryan Stutsman <stutsman@cs.stanford.edu>
2009-12-14 20:19:16 -0800

    Start of simple Haskell bindings

---
+++ bindings/haskell/RAMCloud.hs
@@ -0,0 +1,80 @@
+{-# INCLUDE <ramcloud/client.h> #-}
+{-# LANGUAGE ForeignFunctionInterface #-}
+module RAMCloud (
+                 withClient
+                , write
+                , read
+                )
+where
+
+import Prelude hiding (read)
+import Foreign.C
+import Foreign.C.String
+import Foreign.Ptr
+import Foreign.Storable
+import Foreign.Marshal.Alloc hiding (free)
+
+data ClientStruct = ClientStruct
+type Client = Ptr ClientStruct
+
+type Table = CULong
+type Key = CULong
+
+-- pure function
+-- "unsafe" means it's slightly faster but can't callback to haskell
+foreign import ccall safe "rc_connect" c_rc_connect :: Client -> IO ()
+foreign import ccall safe "rc_disconnect" c_rc_disconnect :: Client -> IO ()
+foreign import ccall safe "rc_ping" c_rc_ping :: Client -> IO ()
+foreign import ccall safe "rc_write" c_rc_write:: Client -> Table -> Key -> CString -> CULong -> CString -> CULong -> IO ()
+foreign import ccall safe "rc_read" c_rc_read :: Client -> Table -> Key -> CString -> Ptr CULong -> CString -> Ptr CULong -> IO CInt
+foreign import ccall safe "rc_new" c_rc_new:: IO Client
+foreign import ccall safe "rc_free" c_rc_free:: Client -> IO ()
+
+new :: IO Client
+-- TODO(stutsman) check return for NULL
+-- TODO(stutsman) even better, just use malloc or alloca in Haskell
+new = c_rc_new
+
+free :: Client -> IO ()
+-- TODO(stutsman) check return for NULL
+free = c_rc_free
+
+connect :: Client -> IO ()
+-- TODO(stutsman) check return and throw an exception if -1
+connect = c_rc_connect
+
+withClient action = do
+  client <- new
+  connect client
+  action client
+  disconnect client
+  -- TODO FIX THIS - Shouldn't be too hard given new stack style
+  --free client
+
+write :: Client -> Table -> Key -> String -> IO ()
+-- TODO(stutsman) binary data, exceptions
+write client table key dat = withCStringLen dat $ \(cstr, len) ->
+                             -- TODO this will be the index data at some point
+                             withCStringLen "" $ \(icstr, ilen) ->
+                             c_rc_write client table key cstr (fromIntegral len) icstr (fromIntegral ilen)
+
+read :: Client -> Table -> Key -> IO String
+read client table key = withCAStringLen (take max $ repeat ' ') $ \(cstr, cstrlen) ->
+                        -- TODO index data
+                        withCAStringLen "" $ \(icstr, icstrlen) ->
+                        alloca $ \lenp -> do
+                        alloca $ \ilenp -> do
+                          poke lenp $ fromIntegral cstrlen
+                          poke ilenp $ fromIntegral icstrlen
+                          r <- c_rc_read client table key cstr lenp icstr ilenp
+                          if r == -1
+                             then ioError $ userError "RAMCloud: read failed"
+                             else do
+                               len <- peek lenp
+                               peekCAStringLen (cstr, fromIntegral len)
+    where max = 4096
+                              
+disconnect :: Client -> IO ()
+-- TODO(stutsman) check return and throw an exception if -1
+disconnect = c_rc_disconnect
+
---
+++ bindings/haskell/Test.hs
@@ -0,0 +1,23 @@
+module Main where
+
+import Control.Monad
+--import Control.Exception
+
+import RAMCloud as RC
+
+count = 100
+
+main = RC.withClient $ \client -> do
+         let write = RC.write client 0
+         let read = RC.read client 0
+
+         let vals = take count $ zip (iterate (1+) 0) (repeat "Hello, World, from Haskell!") 
+
+         mapM_ (uncurry write) vals
+         putStrLn $ "Wrote " ++ (show count) ++ " values"
+
+         val <- catch (read 1) $ \_ -> return "Read failed"
+         --val' <- read 1
+         -- TODO Still have a problem with multiple reads
+         --vals <- mapM read $ take 10 $ iterate (1+) 0
+         print val
