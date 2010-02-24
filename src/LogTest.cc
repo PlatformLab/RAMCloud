7cfdcbe02353bea8bc2df533afe41c4476a3bab2
Stephen M. Rumble <rumble@cs.stanford.edu>
2010-02-23 21:29:06 -0800

    Checkpoint tests.

--- src/LogTest.cc
+++ src/LogTest.cc
@@ -14,6 +14,7 @@
 
 #include <Common.h>
 #include <Log.h>
+#include <LogTypes.h>
 #include <config.h> // for SEGMENT_SIZE and SEGMENT_COUNT
 
 #include <cppunit/extensions/HelperMacros.h>
@@ -26,6 +27,15 @@ class LogTest : public CppUnit::TestFixture {
     CPPUNIT_TEST(TestIsSegmentLive);
     CPPUNIT_TEST(TestGetSegmentIdOffset);
     CPPUNIT_TEST(TestAppend);
+    CPPUNIT_TEST(TestFree);
+    CPPUNIT_TEST(TestGetMaximumAppend);
+    CPPUNIT_TEST(TestAllocateSegmentId);
+    CPPUNIT_TEST(TestGetEvictionCallback);
+    CPPUNIT_TEST(TestGetSegment);
+    CPPUNIT_TEST(TestNewHead);
+    CPPUNIT_TEST(TestChecksumHead);
+    CPPUNIT_TEST(TestRetireHead);
+    CPPUNIT_TEST(TestAppendAnyType);
     CPPUNIT_TEST_SUITE_END();
     RAMCloud::Log *Log;
     void *LogBase;
@@ -122,7 +132,95 @@ class LogTest : public CppUnit::TestFixture {
     void
     TestFree()
     {
-        
+            
+    }
+
+    void
+    TestRegisterType()
+    {
+        // Tested in TestGetEvictionCallback
+    }
+
+    void
+    TestGetMaximumAppend()
+    {
+        uint64_t ma = Log->getMaximumAppend();
+        CPPUNIT_ASSERT(ma < SEGMENT_SIZE);
+        CPPUNIT_ASSERT(ma > (SEGMENT_SIZE - 100));  // test reasonable bound
+    }
+
+    void
+    TestAllocateSegmentId()
+    {
+        uint64_t id = Log->allocateSegmentId();
+        for (uint64_t i = 1; i < 1000000; i++) {
+            CPPUNIT_ASSERT_EQUAL(id + i, Log->allocateSegmentId()); 
+        }
+    }
+
+    static void
+    EvictionCallback(log_entry_type_t type, const void *p,
+                     uint64_t len, void *cookie) 
+    {
+    }
+
+    void
+    TestGetEvictionCallback()
+    {
+        log_eviction_cb_t cb;
+        int cookie = 1983;
+        void *cookiep;
+
+        CPPUNIT_ASSERT(
+            Log->getEvictionCallback(LOG_ENTRY_TYPE_OBJECT, NULL) == NULL);
+
+        Log->registerType(LOG_ENTRY_TYPE_OBJECT, EvictionCallback, &cookie);
+        cb = Log->getEvictionCallback(LOG_ENTRY_TYPE_OBJECT, &cookiep);
+        CPPUNIT_ASSERT((void *)cb == (void *)EvictionCallback);
+        CPPUNIT_ASSERT(*(int *)cookiep == cookie);
+    }
+
+    void
+    TestGetSegment()
+    {
+        // NB: Present code asserts success, so only sanity-check what should
+        //     work. 
+
+        CPPUNIT_ASSERT(Log->getSegment(LogBase, 0) != NULL);
+        CPPUNIT_ASSERT(Log->getSegment(LogBase, 1) != NULL);
+        CPPUNIT_ASSERT(Log->getSegment(LogBase, SEGMENT_SIZE) != NULL);
+
+        uintptr_t b = (uintptr_t)LogBase;
+        for (int i = 0; i < SEGMENT_COUNT; i++) {
+            CPPUNIT_ASSERT(Log->getSegment((void *)(b + (i * SEGMENT_SIZE)),
+                SEGMENT_SIZE) != NULL);
+        }
+    }
+
+    void
+    TestClean()
+    {
+        // ugh.
+    }
+
+    void
+    TestNewHead()
+    {
+    }
+
+    void
+    TestChecksumHead()
+    {
+    }
+
+    void
+    TestRetireHead()
+    {
+    }
+
+    void
+    TestAppendAnyType()
+    {
     }
 };
 CPPUNIT_TEST_SUITE_REGISTRATION(LogTest);
