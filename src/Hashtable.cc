e0baa9157b49da6c1ab2eb8b98117ba823d02a38
Diego Ongaro <ongaro@cs.stanford.edu>
2010-03-08 16:46:08 -0800

    hashtable: Evict memory allocation code
    I didn't know where to put this stuff, so I created a temporary file for it.
    RAM-48 will take care of that someday.

--- src/Hashtable.cc
+++ src/Hashtable.cc
@@ -18,16 +18,13 @@
 #include <Hashtable.h>
 
 #include <Common.h>
+#include <ugly_memory_stuff.h>
 
 #include <cstdio>
 #include <cstdlib>
 #include <stdint.h>
 #include <cmath>
 #include <cstring>
-
-#include <sys/mman.h>
-#include <unistd.h>
-#include <fcntl.h>
 
 namespace RAMCloud {
 
@@ -126,53 +123,6 @@ Hashtable::StoreSample(uint64_t ticks)
         min_ticks = ticks;
     if (ticks > max_ticks)
         max_ticks = ticks;
-}
-
-static void *
-xmalloc_aligned_xmalloc(uint64_t len)
-{
-    uintptr_t p = (uintptr_t)xmalloc(len + 64);
-    p += (64 - (p & 63));
-    return ((void *)p);
-}
-
-static void *
-xmalloc_aligned_hugetlb(uint64_t len)
-{
-    // TODO(stutsman) need to protect this state once we're threaded -
-    // it might be best to yank it out
-    static uintptr_t p = 0;
-    static uint64_t alloced = 0;
-    const size_t maxmem = 1 * 1024 * 1024 * 1024;
-
-    if (p == 0) {
-        unlink("/mnt/hugetlbshit");
-        int fd = open("/mnt/hugetlbshit", O_CREAT | O_TRUNC | O_RDWR, 0600);
-        if (fd == -1) {
-            perror("open");
-            exit(1);
-        }
-        p = (uintptr_t)mmap(0, maxmem, PROT_READ | PROT_WRITE,
-                            MAP_SHARED, fd, 0);
-        if ((void *)p == MAP_FAILED) {
-            perror("mmap");
-            exit(1);
-        }
-        memset(reinterpret_cast<void *>(p), 0, maxmem);
-        printf("Allocated hugetlb region at %p\n", (void *)p);
-    }
-
-    len += (64 - (len & 63));
-    if (alloced + len > maxmem) {
-        printf("ERROR: allocated over hugetlb space!\n");
-        exit(1);
-    }
-    alloced += len;
-
-    uintptr_t r = p;
-    p += len;
-
-    return ((void *)r);
 }
 
 void *
