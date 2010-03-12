507bb08475eac6f64aac198ffce2fe7d58028dec
Diego Ongaro <ongaro@cs.stanford.edu>
2010-03-12 15:37:58 -0800

    hashtable: Add unit tests for lookupEntry, insert

--- src/HashTableTest.cc
+++ src/HashTableTest.cc
@@ -248,10 +248,136 @@ class HashTableTest : public CppUnit::TestFixture {
     CPPUNIT_TEST(test_destructor);
     CPPUNIT_TEST(test_objectContainsKey_assumptions);
     CPPUNIT_TEST(test_objectContainsKey);
+    CPPUNIT_TEST(test_lookupEntry_notFound);
+    CPPUNIT_TEST(test_lookupEntry_cacheLine0Entry0);
+    CPPUNIT_TEST(test_lookupEntry_cacheLine0Entry7);
+    CPPUNIT_TEST(test_lookupEntry_cacheLine2Entry0);
+    CPPUNIT_TEST(test_lookupEntry_hashCollision);
+    CPPUNIT_TEST(test_insert_cacheLine0Entry0);
+    CPPUNIT_TEST(test_insert_cacheLine0Entry7);
+    CPPUNIT_TEST(test_insert_cacheLine2Entry0);
+    CPPUNIT_TEST(test_insert_cacheLineFull);
     CPPUNIT_TEST_SUITE_END();
     DISALLOW_COPY_AND_ASSIGN(HashTableTest); //NOLINT
 
+    // this makes the lines too long...
+#define seven (HashTable::ENTRIES_PER_CACHE_LINE - 1)
+
+    /**
+     * Insert an array of values into a single-bucket hash table.
+     * \param[in] ht
+     *      A hash table with a single bucket.
+     * \param[in] values
+     *      An array of values to add to the bucket (in order). These need not
+     *      be initialized and will be set counting up from 0.
+     * \param[in] numEnt
+     *      The number of values in \a values.
+     * \param[in] cacheLines
+     *      An array of cache lines to back the bucket with. 
+     * \param[in] numCacheLines
+     *      The number of cache lines in \a cacheLines.
+     */
+    void insertArray(HashTable *ht, uint64_t *values, uint64_t numEnt,
+                     HashTable::CacheLine *cacheLines, uint64_t numCacheLines)
+    {
+        HashTable::CacheLine *cl;
+
+        // clear out all the cache lines
+        cl = &cacheLines[0];
+        while (cl < &cacheLines[numCacheLines]) {
+            for (uint64_t j = 0; j < HashTable::ENTRIES_PER_CACHE_LINE; j++)
+                cl->entries[j].clear();
+            cl++;
+        }
+
+        // chain all the cache lines
+        cl = &cacheLines[0];
+        while (cl < &cacheLines[numCacheLines - 1]) {
+            cl->entries[seven].setChainPointer(cl + 1);
+            cl++;
+        }
+
+        // fill in the "log" entries
+        for (uint64_t i = 0; i < numEnt; i++) {
+            values[i] = i;
+
+            uint64_t littleHash;
+            {
+                uint64_t bigHash;
+                HashTable::hash(i, &bigHash, &littleHash);
+            }
+
+            HashTable::Entry *entry;
+            if (0 < i && i == numEnt - 1 && i % seven == 0)
+                entry = &cacheLines[i / seven - 1].entries[seven];
+            else
+                entry = &cacheLines[i / seven].entries[i % seven];
+            entry->setLogPointer(littleHash, &values[i]);
+        }
+
+        ht->buckets = cacheLines;
+    }
+
+    /**
+     * Common setup code for the lookupEntry and insert tests. This mostly
+     * declares variables on the stack, so it's a macro.
+     */
+#define SETUP(_numEnt)  \
+    uint64_t numEnt = _numEnt; \
+    uint64_t numCacheLines; \
+    numCacheLines = ((numEnt + HashTable::ENTRIES_PER_CACHE_LINE - 2) / \
+                              (HashTable::ENTRIES_PER_CACHE_LINE - 1)); \
+    if (numCacheLines == 0) \
+        numCacheLines = 1; \
+    HashTable ht(1); \
+    uint64_t values[numEnt]; \
+    HashTable::CacheLine cacheLines[numCacheLines]; \
+    insertArray(&ht, values, numEnt, cacheLines, numCacheLines)
+
+    /**
+     * Find an entry in a single-bucket hash table by position.
+     * \param[in] ht
+     *      A hash table with a single bucket.
+     * \param[in] x
+     *      The number of the cache line in the chain, starting from 0.
+     * \param[in] y
+     *      The number of the entry in the cache line, starting from 0.
+     * \return
+     *      The entry at \a x and \a y in the only bucket of \a ht.
+     */
+    HashTable::Entry& entryAt(HashTable *ht, uint64_t x, uint64_t y)
+    {
+        HashTable::CacheLine *cl = &ht->buckets[0];
+        while (x > 0) {
+            cl = cl->entries[seven].getChainPointer();
+            x--;
+        }
+        return cl->entries[y];
+    }
+
+    /**
+     * Ensure an entry in a single-bucket hash table contains a given pointer.
+     * \param[in] ht
+     *      A hash table with a single bucket.
+     * \param[in] x
+     *      The number of the cache line in the chain, starting from 0.
+     * \param[in] y
+     *      The number of the entry in the cache line, starting from 0.
+     * \param[in] ptr
+     *      The pointer that we expect to find at the given position.
+     */
+    void assertEntryIs(HashTable *ht, uint64_t x, uint64_t y, uint64_t *ptr)
+    {
+        uint64_t bigHash;
+        uint64_t littleHash;
+        HashTable::hash(*ptr, &bigHash, &littleHash);
+        HashTable::Entry& entry = entryAt(ht, x, y);
+        CPPUNIT_ASSERT(entry.hashMatches(littleHash));
+        CPPUNIT_ASSERT_EQUAL(entry.getLogPointer(), static_cast<void*>(ptr));
+    }
+
   public:
+
     HashTableTest()
     {
     }
@@ -335,6 +461,128 @@ class HashTableTest : public CppUnit::TestFixture {
         CPPUNIT_ASSERT(!HashTable::objectContainsKey(&o, 0UL));
         CPPUNIT_ASSERT(!HashTable::objectContainsKey(&o, 4UL));
         CPPUNIT_ASSERT(HashTable::objectContainsKey(&o, o));
+    }
+
+    /**
+     * Test #RAMCloud::HashTable::lookupEntry() when the key is not found.
+     */
+    void test_lookupEntry_notFound()
+    {
+        {
+            SETUP(0);
+            CPPUNIT_ASSERT_EQUAL(ht.lookupEntry(numEnt + 1),
+                                 reinterpret_cast<HashTable::Entry*>(NULL));
+        }
+        {
+            SETUP(HashTable::ENTRIES_PER_CACHE_LINE * 5);
+            CPPUNIT_ASSERT_EQUAL(ht.lookupEntry(numEnt + 1),
+                                 reinterpret_cast<HashTable::Entry*>(NULL));
+        }
+    }
+
+    /**
+     * Test #RAMCloud::HashTable::lookupEntry() when the key is found in the
+     * first entry of the first cache line.
+     */
+    void test_lookupEntry_cacheLine0Entry0()
+    {
+        SETUP(1);
+        CPPUNIT_ASSERT_EQUAL(ht.lookupEntry(0), &entryAt(&ht, 0, 0));
+    }
+
+    /**
+     * Test #RAMCloud::HashTable::lookupEntry() when the key is found in the
+     * last entry of the first cache line.
+     */
+    void test_lookupEntry_cacheLine0Entry7()
+    {
+        SETUP(HashTable::ENTRIES_PER_CACHE_LINE);
+        CPPUNIT_ASSERT_EQUAL(ht.lookupEntry(seven), &entryAt(&ht, 0, seven));
+    }
+
+    /**
+     * Test #RAMCloud::HashTable::lookupEntry() when the key is found in the
+     * first entry of the third cache line.
+     */
+    void test_lookupEntry_cacheLine2Entry0()
+    {
+        SETUP(HashTable::ENTRIES_PER_CACHE_LINE * 5);
+
+        // with 8 entries per cache line:
+        // cl0: [ k00, k01, k02, k03, k04, k05, k06, cl1 ]
+        // cl1: [ k07, k09, k09, k10, k11, k12, k13, cl2 ]
+        // cl2: [ k14, k15, k16, k17, k18, k19, k20, cl3 ]
+        // ...
+
+        CPPUNIT_ASSERT_EQUAL(ht.lookupEntry(seven * 2), &entryAt(&ht, 2, 0));
+    }
+
+    /**
+     * Test #RAMCloud::HashTable::lookupEntry() when there is a hash collision
+     * with another Entry.
+     */
+    void test_lookupEntry_hashCollision()
+    {
+        SETUP(1);
+        CPPUNIT_ASSERT_EQUAL(ht.lookupEntry(0), &ht.buckets[0].entries[0]);
+        values[0] = 0x43324890UL;
+        CPPUNIT_ASSERT_EQUAL(ht.lookupEntry(0),
+                             reinterpret_cast<HashTable::Entry*>(NULL));
+    }
+
+    /**
+     * Test #RAMCloud::HashTable::insert() when the first entry of the first
+     * cache line is available.
+     */
+    void test_insert_cacheLine0Entry0()
+    {
+        SETUP(0);
+        uint64_t v = 83UL;
+        ht.insert(83UL, &v);
+        assertEntryIs(&ht, 0, 0, &v);
+    }
+
+    /**
+     * Test #RAMCloud::HashTable::insert() when the last entry of the first
+     * cache line is available.
+     */
+    void test_insert_cacheLine0Entry7()
+    {
+        SETUP(HashTable::ENTRIES_PER_CACHE_LINE - 1);
+        uint64_t v = 83UL;
+        ht.insert(83UL, &v);
+        assertEntryIs(&ht, 0, seven, &v);
+    }
+
+    /**
+     * Test #RAMCloud::HashTable::insert() when the first entry of the third
+     * cache line is available. The third cache line is already chained onto
+     * the second.
+     */
+    void test_insert_cacheLine2Entry0()
+    {
+        SETUP(HashTable::ENTRIES_PER_CACHE_LINE * 2);
+        cacheLines[2].entries[0].clear();
+        cacheLines[2].entries[1].clear();
+        uint64_t v = 83UL;
+        ht.insert(83UL, &v);
+        assertEntryIs(&ht, 2, 0, &v);
+    }
+
+    /**
+     * Test #RAMCloud::HashTable::insert() when the first and only cache line
+     * is full. The second cache line needs to be allocated.
+     */
+    void test_insert_cacheLineFull()
+    {
+        SETUP(HashTable::ENTRIES_PER_CACHE_LINE);
+        uint64_t v = 83UL;
+        ht.insert(83UL, &v);
+        CPPUNIT_ASSERT(entryAt(&ht, 0, seven).isChainLink());
+        CPPUNIT_ASSERT(entryAt(&ht, 0, seven).getChainPointer() !=
+                       &cacheLines[1]);
+        assertEntryIs(&ht, 1, 0, &values[seven]);
+        assertEntryIs(&ht, 1, 1, &v);
     }
 
 };
