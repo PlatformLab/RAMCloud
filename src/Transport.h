14c18cc664655402b5d6b4457c0c02b26caac18e
Diego Ongaro <ongaro@cs.stanford.edu>
2010-05-03 17:30:32 -0700

    Finish docs for Transport class

--- src/Transport.h
+++ src/Transport.h
@@ -15,7 +15,7 @@
 
 /**
  * \file
- * Header file for the Transport class.
+ * Header file for the RAMCloud::Transport class and its exceptions.
  */
 
 #ifndef RAMCLOUD_TRANSPORT_H
@@ -29,7 +29,15 @@
 
 namespace RAMCloud {
 
-// stolen from BackupException
+/**
+ * An exception that is thrown when the Transport class encounters a transient
+ * error.
+ */
+
+// This implementation was copied from BackupException.
+// TODO(ongaro): We should create some common infrastructure for exceptions.
+
+// TODO(ongaro): Move inside Transport.
 struct TransportException {
     explicit TransportException() : message(""), errNo(0) {}
     explicit TransportException(std::string msg)
@@ -52,6 +60,16 @@ struct TransportException {
     std::string message;
     int errNo;
 };
+
+/**
+ * An exception that is thrown when the Transport class encounters a fatal
+ * error. This is not recoverable unless you have another Transport to fall
+ * back to, but it's also not a failed assertion because it depends on the
+ * configuration of the outside world. If you're trying to handle these
+ * gracefully, you're probably doing something wrong.
+ */
+
+// TODO(ongaro): Move inside Transport.
 
 struct UnrecoverableTransportException {
     explicit UnrecoverableTransportException() : message(""), errNo(0) {}
@@ -78,12 +96,17 @@ struct UnrecoverableTransportException {
     int errNo;
 };
 
+/**
+ * Best-effort reliable communication with the outside world.
+ */
 class Transport {
 
   protected:
 
     /**
-     * Base class for tokens used in #serverRecv() and #serverSend().
+     * Base class for server tokens used in #serverRecv() and #serverSend().
+     * This class and its derivatives should always live inside a
+     * Transport::ServerToken container.
      */
     class BaseServerToken {
       public:
@@ -94,7 +117,9 @@ class Transport {
     };
 
     /**
-     * Base class for tokens used in #clientSend() and #clientRecv().
+     * Base class for client tokens used in #clientSend() and #clientRecv().
+     * This class and its derivatives should always live inside a
+     * Transport::ClientToken container.
      */
     class BaseClientToken {
       public:
@@ -106,52 +131,121 @@ class Transport {
 
   private:
 
+    /**
+     * A template for an opaque container that holds a derivative of
+     * BaseServerToken or BaseClientToken.
+     * The two classes generated from this template are Transport::ServerToken
+     * and Transport::ClientToken.
+     *
+     * The size of the storage for the concrete token must be at least as big
+     * as the size of the concrete token. You should assert this using
+     * #BUF_SIZE for every concrete token type.
+     *
+     * \tparam C
+     *      Either BaseServerToken or BaseClientToken. This is a concrete token
+     *      that doesn't do anything.
+     */
+
+    // TODO(ongaro): Move the 7 lines of implementation to a Transport.cc?
+
+    // TODO(ongaro): Unit test this.
+
     template <class C>
     class Token {
       public:
+
+        /**
+         * Constructor for Token.
+         * Your token starts out as an instance of \a C, effectively a no-op.
+         */
         Token() {
             static_assert(BUF_SIZE >= sizeof(C));
             new(buf) C();
         }
 
+        /**
+         * Destructor for Token.
+         * Calls the destructor for the concrete token.
+         */
         ~Token() {
             reinterpret_cast<C*>(buf)->~C();
         }
 
+        /**
+         * Destroy the existing concrete token and reset to an instance of \a C.
+         */
         void reinit() {
             reinit<C>();
         }
 
+        /**
+         * Destroy the existing concrete token and construct a new one.
+         *
+         * No arguments will be passed to \a T's constructor.
+         *
+         * Only concrete Transport implementations should call this.
+         *
+         * \tparam T
+         *      A derivative of \a C. It better fit within #BUF_SIZE bytes.
+         * \return
+         *      A pointer to the newly constructed concrete token.
+         */
         template <typename T>
         T* reinit() {
             reinterpret_cast<C*>(buf)->~C();
             return new(buf) T();
         }
 
+        /**
+         * Get a pointer to the concrete token contained within.
+         * \tparam T
+         *      The type of the concrete token. You'll get very little type
+         *      safety here, so be careful.
+         * \return
+         *      See above.
+         */
         template <typename T>
         T* getBuf() {
             return static_cast<T*>(reinterpret_cast<C*>(buf));
         }
 
+        /**
+         * The maximum size in bytes for the concrete token contained within.
+         */
         enum { BUF_SIZE = 32 };
 
       private:
+
+        /**
+         * The actual storage for the concrete token contained within.
+         */
         char buf[BUF_SIZE];
+
+        DISALLOW_COPY_AND_ASSIGN(Token);
     };
 
   public:
 
     /**
-     * Opaque container for any server token type derived from BaseServerToken.
+     * An opaque container for any server token type derived from BaseServerToken.
+     * You'll need one of these for #serverRecv() and #serverSend().
      */
     typedef Token<BaseServerToken> ServerToken;
 
     /**
-     * Opaque container for any client token type derived from BaseClientToken.
+     * An opaque container for any client token type derived from BaseClientToken.
+     * You'll need one of these for #clientSend() and #clientRecv().
      */
     typedef Token<BaseClientToken> ClientToken;
 
+    /**
+     * Constructor for Transport.
+     */
     Transport() {}
+
+    /**
+     * Destructor for Transport.
+     */
     virtual ~Transport() {}
 
     /**
