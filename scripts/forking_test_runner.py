eaf97c7762a4b22ed4da266bf001eb869a097445
Diego Ongaro <ongaro@cs.stanford.edu>
2010-10-04 12:14:05 -0700

    Add script that runs unit tests in separate processes
    Also added RingTest to MakefragTest and moved CClientTest and
    CheckSumTest inside the RAMCloud namespace.

---
+++ scripts/forking_test_runner.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# Copyright (c) 2010 Stanford University
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR(S) DISCLAIM ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL AUTHORS BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+"""Runs each unit test in a separate process.
+
+This is useful for finding which tests cause crashes or enter infinite loops.
+"""
+import os
+import re
+import signal
+import subprocess
+import time
+
+FAIL_AFTER_SECONDS = 5.0
+
+ignore = \
+"""terminate called after throwing an instance of 'std::invalid_argument'
+  what():  No test named <%s> found in test <All Tests>."""
+
+cppunit_fail_header = """!!!FAILURES!!!
+Test Results:
+Run:  1   Failures: 1   Errors: 0
+
+
+1) test: RAMCloud::%s::%s (F) """
+
+signals = dict([(getattr(signal, name), name)
+                for name in dir(signal) if name.startswith('SIG')])
+
+p = subprocess.Popen(['git', 'symbolic-ref', '-q', 'HEAD'],
+                     stdout=subprocess.PIPE)
+p.wait()
+git_branch = re.search('^refs/heads/(.*)$', p.stdout.read())
+if git_branch is None:
+    obj_dir = 'obj'
+else:
+    git_branch = git_branch.group(1)
+    obj_dir = 'obj.%s' % git_branch
+
+tests = []
+for name in os.listdir('src/'):
+    if name.endswith('Test.in.cc') or name.endswith('Test.cc'):
+        suite = None
+        for line in open('src/%s' % name):
+            m = re.match('\s*CPPUNIT_TEST_SUITE\((.*)\);', line)
+            if m:
+                suite = m.group(1)
+                continue
+            m = re.match('\s*CPPUNIT_TEST\((.*)\);', line)
+            if m:
+                test = m.group(1)
+                tests.append((suite, test))
+                continue
+
+print 'Running %d tests...' % len(tests)
+
+ok = 0
+failed = 0
+for (suite, test) in tests:
+    process = subprocess.Popen(['./%s/test' % obj_dir,
+                                '-t', 'RAMCloud::%s::%s' % (suite, test)],
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.STDOUT)
+    start = time.time()
+    now = start
+    rc = None
+    while True:
+        rc = process.poll()
+        if rc is not None:
+            break
+        if time.time() - start > FAIL_AFTER_SECONDS:
+            break
+    if rc != 0:
+        output = process.stdout.read().strip()
+        if output == (ignore % test):
+            print "Ignored: RAMCloud::%s::%s" % (suite, test)
+            continue
+        if rc is None:
+            why = ' by taking too long (over %ss)' % FAIL_AFTER_SECONDS
+        elif rc == 1:
+            why = '' # usual CPPUNIT failure
+        elif rc > 1:
+            why = ' with return value %d' % rc
+        elif rc < 0:
+            why = ' from signal %s' % signals[-rc]
+        cfh = cppunit_fail_header % (suite, test)
+        if output.startswith(cfh):
+            output = output[len(cfh):]
+        print '%s::%s failed%s%s\n' % (suite, test, why,
+                                     ':\n%s' % output if output else '')
+        failed += 1
+    else:
+        ok += 1
+
+print '%d tests passed, %d failed' % (ok, failed)
