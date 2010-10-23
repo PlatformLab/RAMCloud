PlatformLab/RAMCloud

commit b582b1265e8d2328ff08410d6dafb1d4814fc952

Fix generateRandom() to return 64 bits of randomness
Closes RAM-116.

src/Common.h
@@ -262,13 +262,28 @@ class MockTSC {
     }
 };
 __inline __attribute__((always_inline, no_instrument_function))
-long generateRandom(void); // NOLINT
-long // NOLINT
+uint64_t generateRandom(void);
+uint64_t
 generateRandom()
 {
     if (mockRandomValue)
         return mockRandomValue;
-    return random();
+    // Each call to random returns 31 bits of randomness,
+    // so we need three to get 64 bits of randomness.
+    union {
+        struct {
+            uint64_t one:31;
+            uint64_t two:31;
+            uint64_t three:2;
+        };
+        uint64_t all;
+    } r;
+    static_assert(RAND_MAX >= (1 << 31));
+    r.all = 0;
+    r.one = random(); // NOLINT
+    r.two = random(); // NOLINT
+    r.three = random(); // NOLINT
+    return r.all;
 }
 class MockRandom {
     uint64_t original;

cpplint.py
@@ -151,6 +151,7 @@ _ERROR_CATEGORIES = '''\
   ramcloud/catch_ref
   ramcloud/include
   ramcloud/cppunit
+  ramcloud/random
   build/class
   build/deprecated
   build/endif_comment
@@ -2469,6 +2470,11 @@ def CheckLanguage(filename, clean_lines, linenum, file_extension, include_state,
   if Search(r'\bsscanf\b', line):
     error(filename, linenum, 'runtime/printf', 1,
           'sscanf can be ok, but is slow and can overflow buffers.')
+
+  if (Search(r'\b(random|rand|rand_r|\wrand48)\(', line) and
+      not Search(r'\bNOLINT\b', line)):
+      error(filename, linenum, 'ramcloud/random', 5,
+            'Use generateRandom() rather than random(), rand(), etc')
 
   # Check if some verboten operator overloading is going on
   # TODO(unknown): catch out-of-line unary operator&:

src/Bench.cc
@@ -133,7 +133,7 @@ readMany()
     serverCounter = 0;
     for (uint64_t i = 0; i < count; i++) {
         RC::Buffer value;
-        key = randomReads ? rand() % count : i;
+        key = randomReads ? generateRandom() % count : i;
         client->read(table, multirow ? key : 0, &value);
         serverCounter += client->counterValue;
     }

src/CommonTest.cc
@@ -1,0 +1,41 @@
+/* Copyright (c) 2010 Stanford University
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR(S) DISCLAIM ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL AUTHORS BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include "TestUtil.h"
+#include "Common.h"
+
+namespace RAMCloud {
+
+class CommonTest : public CppUnit::TestFixture {
+    CPPUNIT_TEST_SUITE(CommonTest);
+    CPPUNIT_TEST(test_generateRandom);
+    CPPUNIT_TEST_SUITE_END();
+
+  public:
+    CommonTest() {}
+
+    // make sure generateRandom() uses all 64 bits
+    void test_generateRandom() {
+        uint64_t r = 0;
+        for (uint32_t i = 0; i < 50; i++)
+            r |= generateRandom();
+        CPPUNIT_ASSERT_EQUAL(~0UL, r);
+    }
+
+    DISALLOW_COPY_AND_ASSIGN(CommonTest);
+};
+CPPUNIT_TEST_SUITE_REGISTRATION(CommonTest);
+
+}  // namespace RAMCloud

src/CoordinatorServer.h
@@ -30,7 +30,7 @@ namespace RAMCloud {
  */
 class CoordinatorServer : public Server {
   public:
-    CoordinatorServer() : nextServerId(random()) {}
+    CoordinatorServer() : nextServerId(generateRandom()) {}
     virtual ~CoordinatorServer() {}
     void run();
 

src/FastTransport.cc
@@ -186,7 +186,9 @@ FastTransport::sendPacket(const Driver::Address* address,
                           Buffer::Iterator* payload)
 {
 #if TESTING
-    header->pleaseDrop = (generateRandom() % 100) < PACKET_LOSS_PERCENTAGE;
+    // both sides have +1 to silence gcc when PACKET_LOSS_PERCENTAGE is 0
+    header->pleaseDrop = ((generateRandom() % 100) + 1 <
+                          PACKET_LOSS_PERCENTAGE + 1);
 #endif
     driver->sendPacket(address, header, sizeof(*header), payload);
 }

src/HashTableTest.cc
@@ -492,7 +492,7 @@ class HashTableTest : public CppUnit::TestFixture {
         uint64_t observedBits = 0UL;
         srand(1);
         for (uint32_t i = 0; i < 50; i++) {
-            uint64_t input = rand();
+            uint64_t input = generateRandom();
             observedBits |= HashTable::hash(input);
         }
         CPPUNIT_ASSERT_EQUAL(~0UL, observedBits);

src/MakefragTest
@@ -13,6 +13,7 @@ TESTS_SRCFILES := \
 		  src/ChecksumTest.cc \
 		  src/ClientExceptionTest.cc \
 		  src/ClientTest.cc \
+		  src/CommonTest.cc \
 		  src/CoordinatorServerTest.cc \
 		  src/CoordinatorTest.cc \
 		  src/FastTransportTest.cc \

commit f0dc1809f20cbbe6bf76158faa732658fc50b717

Fix build with TESTING set to false
My commit b582b1265e8d2328ff08410d6dafb1d4814fc952 broke it.

src/Common.cc
@@ -76,6 +76,27 @@ debug_dump64(Buffer& buffer)
     debug_dump64(buffer.getRange(0, length), length);
 }
 
+uint64_t
+_generateRandom()
+{
+    // Each call to random returns 31 bits of randomness,
+    // so we need three to get 64 bits of randomness.
+    union {
+        struct {
+            uint64_t one:31;
+            uint64_t two:31;
+            uint64_t three:2;
+        };
+        uint64_t all;
+    } r;
+    static_assert(RAND_MAX >= (1 << 31));
+    r.all = 0;
+    r.one = random(); // NOLINT
+    r.two = random(); // NOLINT
+    r.three = random(); // NOLINT
+    return r.all;
+}
+
 /**
  * Pin the process to a particular CPU.
  * \param cpu

src/Common.h
@@ -216,6 +216,10 @@ _rdpmc(uint32_t counter)
     return ((uint64_t) lo) | (((uint64_t) hi) << 32);
 }
 
+namespace RAMCloud {
+uint64_t _generateRandom();
+}
+
 /// Yield the current task to the scheduler.
 static inline void
 yield()
@@ -268,22 +272,7 @@ generateRandom()
 {
     if (mockRandomValue)
         return mockRandomValue;
-    // Each call to random returns 31 bits of randomness,
-    // so we need three to get 64 bits of randomness.
-    union {
-        struct {
-            uint64_t one:31;
-            uint64_t two:31;
-            uint64_t three:2;
-        };
-        uint64_t all;
-    } r;
-    static_assert(RAND_MAX >= (1 << 31));
-    r.all = 0;
-    r.one = random(); // NOLINT
-    r.two = random(); // NOLINT
-    r.three = random(); // NOLINT
-    return r.all;
+    return RAMCloud::_generateRandom();
 }
 class MockRandom {
     uint64_t original;
@@ -301,6 +290,7 @@ class MockRandom {
 #else
 #define rdtsc() _rdtsc()
 #define rdpmc(c) _rdpmc(c)
+#define generateRandom() RAMCloud::_generateRandom()
 #endif
 
 #if TESTING