88fad060ddf9012d44452b99e2008c1ecf0a6bf1
Diego Ongaro <ongaro@cs.stanford.edu>
2010-10-23 17:18:26 -0700

    Add pragma value to disable GCC warnings altogether
    This is useful for generated files such as those produced by the
    protocol buffers compiler.

--- pragmas.conf.py
+++ pragmas.conf.py
@@ -5,6 +5,7 @@ This is a definitions file for the L{pragmas} module.
 magic_pattern = 'RAMCloud[\s_-]+pragma.*\[(.*)\]'
 
 gccwarn = PragmaDefinition('GCC warnings', default=9)
+gccwarn[0] = 'no warnings'
 gccwarn[5] = 'non-fatal warnings'
 gccwarn[9] = 'fatal, pedantic warnings'
 definitions['GCCWARN'] = gccwarn
