a0b56c24ece0b88ff81f0123ca5361e7ed21bcfb
Stephen M. Rumble <rumble@cs.stanford.edu>
2010-11-01 14:24:00 -0700

    Add simple benchmarks of rabinpoly (used for checksums) and memcpy (used when we aren't zero-copying and to append into the log).
    On our 2.4Ghz E5620 westmeres rabinpoly is about 2ns/byte and
    memcpy is about 1.2ns/byte with an uncached 1k buffer and 0.07ns/byte
    if its in cache.

---
+++ src/misc/memcpy.cc
@@ -0,0 +1,71 @@
+/* Copyright (c) 2010 Stanford University
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR(S) DISCLAIM ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL AUTHORS BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/**
+ * \file
+ * Benchmark for memcpy.
+ */
+
+// RAMCloud pragma [CPPLINT=0]
+
+#include <Common.h>
+#include <BenchUtil.h>
+
+static void
+measure(int bytes, bool cached)
+{
+    uint8_t *src = reinterpret_cast<uint8_t *>(xmalloc(bytes)); 
+    uint8_t *dst = reinterpret_cast<uint8_t *>(xmalloc(bytes));
+    uint8_t *scrub = reinterpret_cast<uint8_t *>(xmalloc(64 * 1024 * 1024));
+
+    // the very first memcpy seems to have a big overhead, so burn though
+    // it before timing
+    memcpy(dst, src, bytes);
+
+    // wreck the cache
+    if (!cached) {
+        for (int i = 0; i < 64 * 1024 * 1024; i++)
+            scrub[i]++;
+    }
+
+    // now time it
+    uint64_t before = rdtsc();
+    memcpy(dst, src, bytes);
+    uint64_t after = rdtsc();
+
+    printf("%10d bytes: %10llu ticks    %10llu nsec    %.2f nsec/byte\n",
+        bytes,
+        after - before,
+        RAMCloud::cyclesToNanoseconds(after - before),
+        (double)RAMCloud::cyclesToNanoseconds(after - before) / bytes);
+
+    free(src);
+    free(dst);
+    free(scrub);
+}
+
+int
+main()
+{
+    printf("=== Cached Memcpy ===\n");
+    for (int i = 16; i <= (16 * 1024 * 1024); i *= 2)
+        measure(i, true);
+
+    printf("\n=== Uncached Memcpy ===\n");
+    for (int i = 16; i <= (16 * 1024 * 1024); i *= 2)
+        measure(i, false);
+
+    return 0;
+}
---
+++ src/misc/rabinpoly.cc
@@ -0,0 +1,69 @@
+/* Copyright (c) 2010 Stanford University
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR(S) DISCLAIM ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL AUTHORS BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/**
+ * \file
+ * Benchmark for dm's rabinpoly code.
+ * on.
+ */
+
+// RAMCloud pragma [CPPLINT=0]
+
+#include <Common.h>
+#include <BenchUtil.h>
+#include <rabinpoly.h>
+
+static const uint64_t  RABIN_POLYNOMIAL = 0x92d42091a28158a5ull;
+
+static void
+measure(int bytes)
+{
+    uint8_t *array = reinterpret_cast<uint8_t *>(xmalloc(bytes)); 
+    rabinpoly rp(RABIN_POLYNOMIAL);
+    uint64_t checksum = 0;
+
+    // randomize input
+    for (int i = 0; i < bytes; i++)
+        array[i] = generateRandom();
+
+    // warm up
+    for (int i = 0; i < bytes; i++)
+        checksum = rp.append8(checksum, array[i]);
+
+    // run the test. be sure method call isn't removed by the compiler.
+    uint64_t before = rdtsc();
+    for (int i = 0; i < bytes; i++)
+        checksum = rp.append8(checksum, array[i]);
+    uint64_t after = rdtsc();
+
+    printf("%10d bytes: %10llu ticks    %10llu nsec    %3llu nsec/byte   "
+        "cksum 0x%16lx\n",
+        bytes,
+        after - before,
+        RAMCloud::cyclesToNanoseconds(after - before),
+        RAMCloud::cyclesToNanoseconds(after - before) / bytes,
+        checksum);
+
+    free(array);
+}
+
+int
+main()
+{
+    for (int i = 16; i <= (16 * 1024 * 1024); i *= 2)
+        measure(i);
+
+    return 0;
+}
