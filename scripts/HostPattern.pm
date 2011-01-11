d272cf19c726fa0fc0e74d3e13c3e0d666be5164
Nandu Jayakumar <nandu@cs.stanford.edu>
2011-01-10 17:45:40 -0800

    Check-ins for round-2 of tests. Script is more flexible now.

---
+++ scripts/HostPattern.pm
@@ -0,0 +1,59 @@
+package HostPattern;
+
+# Provide a simple pattern reader for interpreting host patterns where
+# these patterns contain numerical ranges.
+# Eg:- [1-3] = 1 2 3
+#      [4-7],11,13 = 4 5 6 7 11 13
+
+use strict;
+use warnings;
+
+
+sub hosts {
+  my (@hosts) = @{$_[0]};
+  my @hostlist;
+
+  while (@hosts) {
+    my $h = shift(@hosts);
+    if ($h =~ /^(.*?)\[([^\]]+)\]([^,{[]*)(,(.*))?$/) {
+      my($pre,$post) = ($1,$3);
+      my @r = split(/,/,$2);
+      for (@r) {
+        if (/^(!?)(\d+)(-(\d+))?$/) {
+          my $low = $2;
+          my $high = $3 ? $4 : $low;
+          for (my $n = $low; $n <= $high; $n++) {
+            my $hn = $pre.$n.$post;
+            if ($1) {
+              @hostlist = grep { $_ ne $hn } @hostlist;
+              @hosts = grep { $_ ne $hn } @hosts;
+            } else {
+              push(@hosts,$hn);
+            }
+          }
+        } else {
+          die "Unrecognized host pattern: $h";
+        }
+      }
+      if ($4) {
+        push(@hosts,$5);
+      }
+    } elsif ($h =~ /^(.*?)\{([^\]]+)\}([^,[{]*)(,(.*))?$/) {
+      my($pre,$post) = ($1,$3);
+      my @r = split(/,/,$2);
+      for (@r) {
+        push(@hosts,$pre.$_.$post);
+      }
+      if ($4) {
+        push(@hosts,$5);
+      }
+    } else {
+      my @h = split(/,/,$h);
+      push(@hostlist,@h);
+    }
+  }
+
+  return \@hostlist;
+}
+           
+1;
