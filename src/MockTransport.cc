PlatformLab/RAMCloud

commit 944b63c656450cd0ece3811470314bf81220a172

Modified TransportManager to work in a threaded environment: when Session::clientSend is invoked in a worker thread, it must be synchronized with the dispatch thread.
Also made a few other small cleanups.

src/MockTransport.cc
@@ -26,13 +26,14 @@ namespace RAMCloud {
  * Construct a MockTransport.
  */
 MockTransport::MockTransport(const ServiceLocator *serviceLocator)
-            : outputLog(),
-              inputMessage(NULL),
-              serverRecvCount(0),
-              serverSendCount(0),
-              clientSendCount(0),
-              clientRecvCount(0),
-              locatorString()
+            : outputLog()
+            , inputMessage(NULL)
+            , serverRecvCount(0)
+            , serverSendCount(0)
+            , clientSendCount(0)
+            , clientRecvCount(0)
+            , sessionDeleteCount(0)
+            , locatorString()
 {
     if (serviceLocator != NULL)
         locatorString = serviceLocator->getOriginalString();
@@ -74,6 +75,13 @@ MockTransport::getSession()
     return new MockSession(this);
 }
 
+/**
+ * Destructor for MockSession: just log the destruction.
+ */
+MockTransport::MockSession::~MockSession()
+{
+    transport->sessionDeleteCount++;
+}
 
 /**
  * Issue an RPC request using this transport.
@@ -90,7 +98,8 @@ MockTransport::getSession()
  *          space in this Allocation.
  */
 Transport::ClientRpc*
-MockTransport::MockSession::clientSend(Buffer* payload, Buffer* response) {
+MockTransport::MockSession::clientSend(Buffer* payload, Buffer* response)
+{
     if (transport->outputLog.length() != 0) {
         transport->outputLog.append(" | ");
     }
@@ -109,7 +118,8 @@ MockTransport::MockSession::clientSend(Buffer* payload, Buffer* response) {
  *      in the format expected by Buffer::fillFromString.
  */
 void
-MockTransport::setInput(const char* s) {
+MockTransport::setInput(const char* s)
+{
     inputMessage = s;
 }
 
@@ -139,7 +149,8 @@ MockTransport::MockServerRpc::MockServerRpc(MockTransport* transport,
  * the reply message and deletes this reply object.
  */
 void
-MockTransport::MockServerRpc::sendReply() {
+MockTransport::MockServerRpc::sendReply()
+{
     if (transport->outputLog.length() != 0) {
         transport->outputLog.append(" | ");
     }
@@ -170,7 +181,8 @@ MockTransport::MockClientRpc::MockClientRpc(MockTransport* transport,
  * by the current test (or an empty buffer, if nothing was supplied).
  */
 void
-MockTransport::MockClientRpc::wait() {
+MockTransport::MockClientRpc::wait()
+{
     // The call to fillFromString below will overwrite everything in the
     // response buffer, including this MockClientRpc object; pull out of
     // the object anything we will need.

src/MockTransport.h
@@ -74,6 +74,7 @@ class MockTransport : public Transport {
             MockSession(MockTransport* transport,
                         const ServiceLocator& serviceLocator)
                 : transport(transport), serviceLocator(serviceLocator) {}
+            virtual ~MockSession();
             virtual ClientRpc* clientSend(Buffer* payload, Buffer* response);
             virtual void release() {
                 delete this;
@@ -101,6 +102,7 @@ class MockTransport : public Transport {
     uint32_t serverSendCount;
     uint32_t clientSendCount;
     uint32_t clientRecvCount;
+    uint32_t sessionDeleteCount;
 
     // ServiceLocator string passed to constructor, or empty if the
     // constructor argument was NULL.

src/TransportManagerTest.cc
@@ -20,132 +20,189 @@
 
 namespace RAMCloud {
 
-class TransportManagerTest : public CppUnit::TestFixture {
-    CPPUNIT_TEST_SUITE(TransportManagerTest);
-    CPPUNIT_TEST(test_initialize);
-    CPPUNIT_TEST(test_getSession);
-    CPPUNIT_TEST(test_serverRecv);
-    CPPUNIT_TEST(test_getListeningLocators);
-    CPPUNIT_TEST(test_getListeningLocatorsString);
-    CPPUNIT_TEST_SUITE_END();
-
+class TransportManagerTest : public ::testing::Test {
   public:
     TransportManagerTest() {}
-
-    void test_initialize() {
-        static struct MockTransportFactory : public TransportFactory {
-            MockTransportFactory() : TransportFactory("mock") {}
-            Transport* createTransport(const ServiceLocator* local) {
-                return new MockTransport();
-            }
-        } mockTransportFactory;
-
-        // If "mockThrow:" is _not_ in the service locator, that should be
-        // caught and the transport ignored. If it is, then any exception
-        // is an error and should propagate up.
-        static struct MockThrowTransportFactory : public TransportFactory {
-            MockThrowTransportFactory() : TransportFactory("mockThrow") {}
-            Transport* createTransport(const ServiceLocator* local) {
-                throw TransportException(HERE, "boom!");
-            }
-        } mockThrowTransportFactory;
-
-        TransportManager manager;
-        manager.transportFactories.insert(&mockTransportFactory);
-        manager.transportFactories.insert(&mockThrowTransportFactory);
-        manager.initialize("foo:; mock:; bar:");
-        CPPUNIT_ASSERT_EQUAL(1, manager.listening.size());
-        CPPUNIT_ASSERT(manager.transports.size() > 0);
-
-        TransportManager manager2;
-        manager2.transportFactories.insert(&mockThrowTransportFactory);
-        CPPUNIT_ASSERT_THROW(manager2.initialize(
-            "foo:; mock:; bar:; mockThrow:"), TransportException);
-    }
-
-    void test_getSession() {
-        TransportManager manager;
-        MockTransport* mock1 = new MockTransport();
-        manager.registerMock(mock1);
-        mock1->setInput("x");
-        CPPUNIT_ASSERT_THROW(manager.getSession("foo:"),
-                             TransportException);
-        Transport::SessionRef session(manager.getSession("foo:;mock:"));
-        CPPUNIT_ASSERT(session.get() != NULL);
-        Transport::SessionRef session2(manager.getSession("foo:;mock:"));
-        CPPUNIT_ASSERT(session.get() == session2.get());
-    }
-
-    void test_serverRecv() {
-        Transport::ServerRpc* rpc;
-
-        TransportManager manager;
-
-        CPPUNIT_ASSERT_THROW(manager.serverRecv(), TransportException);
-
-        MockTransport* mock1 = new MockTransport();
-        MockTransport* mock2 = new MockTransport();
-        MockTransport* mock3 = new MockTransport();
-        manager.registerMock(mock1);
-        manager.registerMock(mock2);
-        manager.registerMock(mock3);
-
-        mock3->setInput("y");
-        rpc = manager.serverRecv();
-        CPPUNIT_ASSERT_EQUAL("y",
-            static_cast<const char*>(rpc->recvPayload.getRange(0, 2)));
-        delete rpc;
-
-        mock2->setInput("x");
-        rpc = manager.serverRecv();
-        CPPUNIT_ASSERT_EQUAL("x",
-            static_cast<const char*>(rpc->recvPayload.getRange(0, 2)));
-        delete rpc;
-    }
-
-    void test_getListeningLocators() {
-        TransportManager manager;
-
-        CPPUNIT_ASSERT_THROW(manager.getListeningLocators(),
-            TransportException);
-
-        ServiceLocator mock1sl("hi:");
-        ServiceLocator mock2sl("there:");
-        MockTransport *mock1 = new MockTransport(&mock1sl);
-        MockTransport *mock2 = new MockTransport(&mock2sl);
-        manager.registerMock(mock1);
-        manager.registerMock(mock2);
-
-        CPPUNIT_ASSERT_EQUAL(2, manager.listening.size());
-
-        ServiceLocatorList sll = manager.getListeningLocators();
-        CPPUNIT_ASSERT_EQUAL(2, sll.size());
-        CPPUNIT_ASSERT_EQUAL("hi:", sll[0].getOriginalString());
-        CPPUNIT_ASSERT_EQUAL("there:", sll[1].getOriginalString());
-    }
-
-    void test_getListeningLocatorsString() {
-        TransportManager manager;
-
-        CPPUNIT_ASSERT_THROW(manager.getListeningLocators(),
-            TransportException);
-
-        ServiceLocator mock1sl("hi:");
-        ServiceLocator mock2sl("there:");
-        MockTransport *mock1 = new MockTransport(&mock1sl);
-        MockTransport *mock2 = new MockTransport(&mock2sl);
-
-        manager.initialize("");
-        CPPUNIT_ASSERT_EQUAL("", manager.getListeningLocatorsString());
-
-        manager.registerMock(mock1);
-        CPPUNIT_ASSERT_EQUAL("hi:", manager.getListeningLocatorsString());
-
-        manager.registerMock(mock2);
-        CPPUNIT_ASSERT_EQUAL("hi:;there:",
-            manager.getListeningLocatorsString());
-    }
 };
-CPPUNIT_TEST_SUITE_REGISTRATION(TransportManagerTest);
+
+TEST_F(TransportManagerTest, initialize) {
+    static struct MockTransportFactory : public TransportFactory {
+        MockTransportFactory() : TransportFactory("mock") {}
+        Transport* createTransport(const ServiceLocator* local) {
+            return new MockTransport();
+        }
+    } mockTransportFactory;
+
+    // If "mockThrow:" is _not_ in the service locator, that should be
+    // caught and the transport ignored. If it is, then any exception
+    // is an error and should propagate up.
+    static struct MockThrowTransportFactory : public TransportFactory {
+        MockThrowTransportFactory() : TransportFactory("mockThrow") {}
+        Transport* createTransport(const ServiceLocator* local) {
+            throw TransportException(HERE, "boom!");
+        }
+    } mockThrowTransportFactory;
+
+    TransportManager manager;
+    manager.transportFactories.insert(&mockTransportFactory);
+    manager.transportFactories.insert(&mockThrowTransportFactory);
+    manager.initialize("foo:; mock:; bar:");
+    EXPECT_EQ(1U, manager.listening.size());
+    EXPECT_TRUE(manager.isServer);
+    EXPECT_TRUE(manager.transports.size() > 0);
+
+    TransportManager manager2;
+    manager2.transportFactories.insert(&mockThrowTransportFactory);
+    EXPECT_THROW(manager2.initialize(
+        "foo:; mock:; bar:; mockThrow:"), TransportException);
+
+    TransportManager manager3;
+    manager3.initialize("");
+    EXPECT_FALSE(manager3.isServer);
+}
+
+TEST_F(TransportManagerTest, getSession) {
+    TestLog::Enable _;
+    TransportManager manager;
+    MockTransport* mock1 = new MockTransport();
+    manager.registerMock(mock1);
+    mock1->setInput("x");
+    EXPECT_THROW(manager.getSession("foo:"),
+                            TransportException);
+    Transport::SessionRef session(manager.getSession("foo:;mock:"));
+    EXPECT_TRUE(session.get() != NULL);
+    Transport::SessionRef session2(manager.getSession("foo:;mock:"));
+    EXPECT_TRUE(session.get() == session2.get());
+    EXPECT_STREQ("", TestLog::get().c_str());
+
+    // Verify that WorkerSessions are created when needed.
+    manager.sessionCache.clear();
+    manager.isServer = true;
+    Transport::SessionRef session3(manager.getSession("mock:"));
+    EXPECT_TRUE(session3.get() != NULL);
+    EXPECT_STREQ("WorkerSession: created", TestLog::get().c_str());
+}
+
+TEST_F(TransportManagerTest, serverRecv) {
+    Transport::ServerRpc* rpc;
+
+    TransportManager manager;
+
+    EXPECT_THROW(manager.serverRecv(), TransportException);
+
+    MockTransport* mock1 = new MockTransport();
+    MockTransport* mock2 = new MockTransport();
+    MockTransport* mock3 = new MockTransport();
+    manager.registerMock(mock1);
+    manager.registerMock(mock2);
+    manager.registerMock(mock3);
+
+    mock3->setInput("y");
+    rpc = manager.serverRecv();
+    EXPECT_STREQ("y",
+        static_cast<const char*>(rpc->recvPayload.getRange(0, 2)));
+    delete rpc;
+
+    mock2->setInput("x");
+    rpc = manager.serverRecv();
+    EXPECT_STREQ("x",
+        static_cast<const char*>(rpc->recvPayload.getRange(0, 2)));
+    delete rpc;
+}
+
+TEST_F(TransportManagerTest, getListeningLocators) {
+    TransportManager manager;
+
+    EXPECT_THROW(manager.getListeningLocators(),
+        TransportException);
+
+    ServiceLocator mock1sl("hi:");
+    ServiceLocator mock2sl("there:");
+    MockTransport *mock1 = new MockTransport(&mock1sl);
+    MockTransport *mock2 = new MockTransport(&mock2sl);
+    manager.registerMock(mock1);
+    manager.registerMock(mock2);
+
+    EXPECT_EQ(2U, manager.listening.size());
+
+    ServiceLocatorList sll = manager.getListeningLocators();
+    EXPECT_EQ(2U, sll.size());
+    EXPECT_STREQ("hi:", sll[0].getOriginalString().c_str());
+    EXPECT_STREQ("there:", sll[1].getOriginalString().c_str());
+}
+
+TEST_F(TransportManagerTest, getListeningLocatorsString) {
+    TransportManager manager;
+
+    EXPECT_THROW(manager.getListeningLocators(),
+        TransportException);
+
+    ServiceLocator mock1sl("hi:");
+    ServiceLocator mock2sl("there:");
+    MockTransport *mock1 = new MockTransport(&mock1sl);
+    MockTransport *mock2 = new MockTransport(&mock2sl);
+
+    manager.initialize("");
+    EXPECT_STREQ("", manager.getListeningLocatorsString().c_str());
+
+    manager.registerMock(mock1);
+    EXPECT_STREQ("hi:", manager.getListeningLocatorsString().c_str());
+
+    manager.registerMock(mock2);
+    EXPECT_STREQ("hi:;there:",
+        manager.getListeningLocatorsString().c_str());
+}
+
+// The following tests both the constructor and the clientSend method.
+TEST_F(TransportManagerTest, workerSession) {
+    MockTransport transport;
+    Buffer request;
+    Buffer reply;
+    request.fillFromString("abcdefg");
+
+    Transport::Session* wrappedSession = new TransportManager::WorkerSession(
+            transport.getSession());
+
+    // Make sure that clientSend gets passed down to the underlying session.
+    wrappedSession->clientSend(&request, &reply);
+    EXPECT_STREQ("clientSend: abcdefg/0", transport.outputLog.c_str());
+    EXPECT_EQ(0U, transport.sessionDeleteCount);
+
+    // Make sure that sessions get cleaned up properly.
+    delete wrappedSession;
+    EXPECT_EQ(1U, transport.sessionDeleteCount);
+}
+
+// The next test makes sure that clientSend synchronizes properly with the
+// dispatch thread.
+
+void worker(Transport::SessionRef session) {
+    Buffer request;
+    Buffer reply;
+    request.fillFromString("abcdefg");
+    session->clientSend(&request, &reply);
+}
+
+TEST_F(TransportManagerTest, workerSessionSyncWithDispatchThread) {
+    Dispatch::reset();
+    Dispatch::setDispatchThread();
+
+    MockTransport transport;
+    Transport::SessionRef wrappedSession = new TransportManager::WorkerSession(
+            transport.getSession());
+    boost::thread child(worker, wrappedSession);
+
+    // Make sure the child hangs in clientSend until we invoke the dispatcher.
+    usleep(1000);
+    EXPECT_STREQ("", transport.outputLog.c_str());
+    for (int i = 0; i < 1000; i++) {
+        Dispatch::poll();
+        if (transport.outputLog.size() > 0) {
+            break;
+        }
+    }
+    EXPECT_STREQ("clientSend: abcdefg/0", transport.outputLog.c_str());
+    child.join();
+}
 
 }  // namespace RAMCloud

src/ServiceManager.cc
@@ -175,12 +175,18 @@ ServiceManager::workerMain(Worker* worker)
         while (worker->state.load() != Worker::WORKING) {
             if (Dispatch::currentTime >= stopPollingTime) {
                 // It's been a long time since we've had any work to do; go
-                // to sleep so we don't waste any more CPU cycles.
-                worker->state.store(Worker::SLEEPING);
-                if (sys->futexWait(reinterpret_cast<int*>(&worker->state),
-                        Worker::SLEEPING) == -1) {
-                    LOG(ERROR, "futexWait failed in ServiceManager::workerMain: %s",
-                            strerror(errno));
+                // to sleep so we don't waste any more CPU cycles.  Tricky
+                // race condition: the dispatch thread could change the state
+                // to WORKING just before we change it to SLEEPING, so use an
+                // atomic op and only change to SLEEPING if the current value
+                // is POLLING.
+                int expected = Worker::POLLING;
+                if (worker->state.compare_exchange_weak(expected, Worker::SLEEPING)) {
+                    if (sys->futexWait(reinterpret_cast<int*>(&worker->state),
+                            Worker::SLEEPING) == -1) {
+                        LOG(ERROR, "futexWait failed in ServiceManager::workerMain: %s",
+                                strerror(errno));
+                    }
                 }
             }
             // Empty loop body.

src/ServiceManager.h
@@ -61,28 +61,33 @@ class Worker {
 
     /// Values for #state:
     enum {
-        POLLING,                       /// Set by the worker thread to
-                                       /// indicate that it has finished
-                                       /// processing its current request
-                                       /// and is in a polling loop waiting
-                                       /// for more work to do.
-        WORKING,                       /// Set by the dispatch thread to
-                                       /// indicate that a new RPC is ready
-                                       /// to be processed.
-        POSTPROCESSING,                /// Set by the worker thread if it
-                                       /// invokes #sendReply on the RPC;
-                                       /// means that the RPC response is
-                                       /// ready to be returned to the client,
-                                       /// but the worker is still busy so
-                                       /// we can't give it anything else
-                                       /// to do.
-        SLEEPING                       /// Set by the worker thread to
-                                       /// indicate that it has been waiting
-                                       /// so long for new work that it put
-                                       /// itself to sleep; the dispatch
-                                       /// thread will need to wake it up the
-                                       /// next time it has an RPC for the
-                                       /// worker.
+        /// Set by the worker thread to indicate that it has finished
+        /// processing its current request and is in a polling loop waiting
+        /// for more work to do.  From this state 2 things can happen:
+        /// * dispatch thread can change state to WORKING
+        /// * worker can change state to SLEEPING.
+        /// Note: this is the only state where both threads may set a new
+        /// state (it requires special care!).
+        POLLING,
+
+        /// Set by the dispatch thread to indicate that a new RPC is ready
+        /// to be processed.  From this state the worker will change state
+        /// to either POSTPROCESSING or POLLING.
+        WORKING,
+
+        /// Set by the worker thread if it invokes #sendReply on the RPC;
+        /// means that the RPC response is ready to be returned to the client,
+        /// but the worker is still busy so we can't give it anything else
+        /// to do.  From the state the worker will eventually change the
+        /// state to POLLING.
+        POSTPROCESSING,
+
+        /// Set by the worker thread to indicate that it has been waiting
+        /// so long for new work that it put itself to sleep; the dispatch
+        /// thread will need to wake it up the next time it has an RPC for the
+        /// worker.  From the state the dispatch thread will eventually change
+        /// state to WORKING.
+        SLEEPING
     };
     bool exited;                       /// True means the worker is no longer
                                        /// running.

src/ServiceTest.cc
@@ -145,7 +145,7 @@ TEST_F(ServiceTest, sendReply) {
     service.delay = true;
     service.sendReply = true;
     MockTransport transport;
-    ServiceManager* manager = new ServiceManager(&service);
+    ServiceManager manager(&service);
     MockTransport::MockServerRpc* rpc = new MockTransport::MockServerRpc(
             &transport, "3 4");
     ServiceManager::handleRpc(rpc);
@@ -154,13 +154,13 @@ TEST_F(ServiceTest, sendReply) {
     // returned yet.
     for (int i = 0; i < 1000; i++) {
         Dispatch::poll();
-        if (manager->worker.rpc == NULL) {
+        if (manager.worker.rpc == NULL) {
             break;
         }
         usleep(1000);
     }
-    EXPECT_EQ((Transport::ServerRpc*) NULL, manager->worker.rpc);
-    EXPECT_EQ(Worker::POSTPROCESSING, manager->worker.state.load());
+    EXPECT_EQ((Transport::ServerRpc*) NULL, manager.worker.rpc);
+    EXPECT_EQ(Worker::POSTPROCESSING, manager.worker.state.load());
     EXPECT_EQ("serverReply: 4 5", transport.outputLog);
     service.delay = false;
 }

src/Transport.h
@@ -189,8 +189,7 @@ class Transport {
          * \throw TransportException
          *      If any errors occur while initiating the request.
          */
-        virtual ClientRpc* clientSend(Buffer* request, Buffer* response)
-            __attribute__((warn_unused_result)) = 0;
+        virtual ClientRpc* clientSend(Buffer* request, Buffer* response) = 0;
 
         /**
          * \return

src/TransportManager.cc
@@ -72,6 +72,7 @@ TransportManager  __attribute__((init_priority(400))) transportManager;
 
 TransportManager::TransportManager()
     : initialized(false)
+    , isServer(false)
     , transportFactories()
     , transports()
     , listening()
@@ -101,17 +102,14 @@ TransportManager::~TransportManager()
 }
 
 /**
- * This method is invoked on servers to construct the individual transports
- * that will be used to send and receive.
- *
- * Calling this method is required before any calls to #serverRecv(), since
- * the receiving transports need to be instantiated with their local addresses
- * first. In this case, it must be called explicitly before any calls to
- * #getSession().
+ * Initialize information about available transports that is used by both
+ * servers and clients.
  *
  * \param localServiceLocator
- *      One or more locators that clients can use to send requests to this
- *      server.
+ *      If the application is a server, this specifies one or more locators
+ *      that clients can use to send requests to this server.  An empty
+ *      string indicates that this application is a client, so it will not be
+ *      receiving any requests.
  */
 void
 TransportManager::initialize(const char* localServiceLocator)
@@ -119,26 +117,32 @@ TransportManager::initialize(const char* localServiceLocator)
     assert(!initialized);
 
     auto locators = ServiceLocator::parseServiceLocators(localServiceLocator);
+    if (locators.size() != 0) {
+        isServer = true;
+    }
 
     foreach (auto factory, transportFactories) {
         Transport* transport = NULL;
         foreach (auto& locator, locators) {
             if (factory->supports(locator.getProtocol().c_str())) {
                 // The transport supports a protocol that we can receive
-                // packets on. Since it is expected that this transport
-                // work, we do not catch exceptions if it is unavailable.
+                // requests on. Since it is expected that this transport
+                // works, we do not catch exceptions if it is unavailable.
                 transport = factory->createTransport(&locator);
                 listening.push_back(transport);
                 goto insert_protocol_mappings;
             }
         }
 
-        // The transport doesn't support any protocols that we can receive
-        // packets on. Such transports need not be available, e.g. if the
-        // physical device (NIC) does not exist.
+        // The transport cannot be used to receive requests (it didn't
+        // support any of the desired locators), but it can still potentially
+        // be used for issuing requests to other servers.
         try {
             transport = factory->createTransport(NULL);
         } catch (TransportException &e) {
+            // Don't get upset if the transport isn't available; it could
+            // be something simple such as the physical device (NIC) does
+            // not exist.
         }
 
  insert_protocol_mappings:
@@ -188,6 +192,9 @@ TransportManager::getSession(const char* serviceLocator)
             auto transport = protocolTransport.second;
             try {
                 auto session = transport->getSession(locator);
+                if (isServer) {
+                    session = new WorkerSession(session);
+                }
 
                 // Only first protocol is used, but the cache is based
                 // on the complete initial service locator string.
@@ -302,6 +309,35 @@ TransportManager::dumpStats()
 {
     foreach (auto transport, transports)
         transport->dumpStats();
+}
+
+/**
+ * Construct a WorkerSession.
+ *
+ * \param wrapped
+ *      Another Session object, to which #clientSend requests will be
+ *      forwarded.
+ */
+TransportManager::WorkerSession::WorkerSession(Transport::SessionRef wrapped)
+    : wrapped(wrapped)
+{
+    TEST_LOG("created");
+}
+
+/**
+ * Construct a WorkerSession.
+ *
+ * \param wrapped
+ *      Another Session object, to which #clientSend requests will be
+ *      forwarded.
+ */
+Transport::ClientRpc*
+TransportManager::WorkerSession::clientSend(Buffer* request, Buffer* reply)
+{
+    // Must make sure that the dispatch thread isn't running when we
+    // invoked the real clientSend.
+    Dispatch::Lock lock;
+    return wrapped->clientSend(request, reply);
 }
 
 } // namespace RAMCloud

commit 89457b2bb28b2065e53dcf1e78fed3267951dd4a

Fixed memory corruption error introduced by commit 944b63c656450.

src/MockTransport.cc
@@ -17,6 +17,7 @@
 #include "MockTransport.h"
 
 namespace RAMCloud {
+uint32_t RAMCloud::MockTransport::sessionDeleteCount = 0;
 
 //------------------------------
 // MockTransport class
@@ -32,7 +33,6 @@ MockTransport::MockTransport(const ServiceLocator *serviceLocator)
             , serverSendCount(0)
             , clientSendCount(0)
             , clientRecvCount(0)
-            , sessionDeleteCount(0)
             , locatorString()
 {
     if (serviceLocator != NULL)
@@ -80,7 +80,7 @@ MockTransport::getSession()
  */
 MockTransport::MockSession::~MockSession()
 {
-    transport->sessionDeleteCount++;
+    MockTransport::sessionDeleteCount++;
 }
 
 /**

src/MockTransport.h
@@ -97,7 +97,11 @@ class MockTransport : public Transport {
     uint32_t serverSendCount;
     uint32_t clientSendCount;
     uint32_t clientRecvCount;
-    uint32_t sessionDeleteCount;
+
+    // The following variable must be static: sessions can get deleted
+    // *after* their transport, so can't reference anything in a particular
+    // transport.
+    static uint32_t sessionDeleteCount;
 
     // ServiceLocator string passed to constructor, or empty if the
     // constructor argument was NULL.

src/TransportManagerTest.cc
@@ -163,6 +163,7 @@ TEST_F(TransportManagerTest, workerSession) {
     Buffer request;
     Buffer reply;
     request.fillFromString("abcdefg");
+    MockTransport::sessionDeleteCount = 0;
 
     Transport::Session* wrappedSession = new TransportManager::WorkerSession(
             transport.getSession());
@@ -170,11 +171,11 @@ TEST_F(TransportManagerTest, workerSession) {
     // Make sure that clientSend gets passed down to the underlying session.
     wrappedSession->clientSend(&request, &reply);
     EXPECT_STREQ("clientSend: abcdefg/0", transport.outputLog.c_str());
-    EXPECT_EQ(0U, transport.sessionDeleteCount);
+    EXPECT_EQ(0U, MockTransport::sessionDeleteCount);
 
     // Make sure that sessions get cleaned up properly.
     delete wrappedSession;
-    EXPECT_EQ(1U, transport.sessionDeleteCount);
+    EXPECT_EQ(1U, MockTransport::sessionDeleteCount);
 }
 
 // The next test makes sure that clientSend synchronizes properly with the