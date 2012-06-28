dbdb44eba0255e50b413f5f5dc645e87d3a5109f
Ankita Kejriwal <ankitak@cs.stanford.edu>
2012-06-28 13:03:10 -0700

    Partial refactoring of CoordinatorService.
    CoorinatorServerManager class handles all server configuration details on
    behalf of the coordinator.
    
    For every coordinator RPC related to server configuration,
    CoordinatorService understands the request and response RPC formats
    and thus processes the RPCs; It calls into CoordinatorServerManager
    that handles the implementation of the function.

--- src/ServerTest.cc
+++ src/ServerTest.cc
@@ -82,7 +82,7 @@ TEST_F(ServerTest, createAndRegisterServices) {
 
 namespace {
 bool enlistServerFilter(string s) {
-    return s == "enlistServer";
+    return s == "enlistServerStart";
 }
 }
 
@@ -90,12 +90,13 @@ TEST_F(ServerTest, enlist) {
     server->createAndRegisterServices(&cluster.transport);
     TestLog::Enable _(enlistServerFilter);
     server->enlist({128, 0});
-    EXPECT_EQ("enlistServer: Enlisting new server at mock:host=server0 "
-                  "(server id 1) supporting services: MASTER_SERVICE, "
-                  "BACKUP_SERVICE, PING_SERVICE, MEMBERSHIP_SERVICE | "
-              "enlistServer: Newly enlisted server 1 replaces server 128 | "
-              "enlistServer: Backup at id 1 has 100 MB/s read 100 MB/s write",
-              TestLog::get());
+    EXPECT_EQ(
+        "enlistServerStart: Enlisting new server at mock:host=server0 "
+        "(server id 1) supporting services: MASTER_SERVICE, "
+        "BACKUP_SERVICE, PING_SERVICE, MEMBERSHIP_SERVICE | "
+        "enlistServerStart: Newly enlisted server 1 replaces server 128 | "
+        "enlistServerStart: Backup at id 1 has 100 MB/s read 100 MB/s write",
+         TestLog::get());
     ASSERT_TRUE(server->master->serverId.isValid());
     EXPECT_TRUE(server->backup->serverId.isValid());
 
