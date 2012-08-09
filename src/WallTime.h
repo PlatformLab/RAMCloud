5e493239f44905ad61904099b71e4f26e8887c73
Stephen M. Rumble <rumble@cs.stanford.edu>
2012-08-09 14:43:05 -0700

    Close RAM-437 by making WallTime.cc's methods static parts of a WallTime class. Add a few unit tests while here to sanity check the clock and it's conversion to unix time.

--- src/WallTime.h
+++ src/WallTime.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011 Stanford University
+/* Copyright (c) 2011-2012 Stanford University
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -23,12 +23,49 @@
 
 namespace RAMCloud {
 
-#if TESTING
-extern uint32_t mockWallTimeValue;
-#endif
+/**
+ * The WallTime class provides a fast, seconds-granularity real time clock.
+ * It works by initially querying the OS for the initial time value, and then
+ * increments that count based on the CPU's timestamp counter in subsequent
+ * calls.
+ *
+ * WallTime clocks are 32-bit, like Unix's time_t, though it's unsigned and the
+ * "RAMCloud" epoch begins much more recently. This should last us until about
+ * the year 2150. 
+ */
+class WallTime {
+  public:
+    static uint32_t secondsTimestamp(void);
+    static time_t secondsTimestampToUnix(uint32_t timestamp);
 
-uint32_t secondsTimestamp(void);
-time_t secondsTimestampToUnix(uint32_t timestamp);
+    /**
+     * The RAMCloud epoch began Jan 1 00:00:00 2011 UTC.
+     * The Unix epoch began 41(!) years prior. May ours
+     * last just as long!
+     */
+    static const uint32_t RAMCLOUD_UNIX_OFFSET = (41 * 365 * 86400);
+
+    /**
+     * The base Unix time, used by the timestamp methods below.
+     * This exists so that we need only call the kernel once to
+     * get the time. In the future, we'll use rdtsc().
+     */
+    static time_t baseTime;
+
+    /**
+     * CPU timestamp counter value at the first call to
+     * #secondsTimestamp. Used to avoid subsequent syscalls
+     * to obtain current time in conjunction with #baseTime.
+     */
+    static uint64_t baseTsc;
+
+#if TESTING
+    /**
+     * For testing only. Used to force the time this class returns.
+     */
+    static uint32_t mockWallTimeValue;
+#endif
+};
 
 }
 
