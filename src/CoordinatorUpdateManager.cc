95bfb4053e998aba5c2b63b67dd7d9ae51c3fc0d
John Ousterhout <ouster@cs.stanford.edu>
2013-12-17 14:18:04 -0800

    Added an assertion to CoordinatorUpdateManager that helps to detect an easy-to-make initialization error in unit tests that can cause crashes.

--- src/CoordinatorUpdateManager.cc
+++ src/CoordinatorUpdateManager.cc
@@ -34,6 +34,7 @@ CoordinatorUpdateManager::CoordinatorUpdateManager(ExternalStorage* storage)
     , externalLastFinished(0)
     , externalFirstAvailable(0)
     , recoveryComplete(false)
+    , initComplete(false)
 {}
 
 /**
@@ -84,6 +85,7 @@ CoordinatorUpdateManager::init()
     }
     smallestUnfinished = externalFirstAvailable;
     lastAssigned = smallestUnfinished - 1;
+    initComplete = true;
     return externalLastFinished;
 }
 
@@ -113,6 +115,12 @@ uint64_t
 CoordinatorUpdateManager::nextSequenceNumber()
 {
     Lock lock(mutex);
+
+    // If the following assertion fails, the most likely cause is that
+    // a test created a CoordinatorService and didn't call
+    // CoordinatorService::waitForInit (which resulted in a race).
+    assert(initComplete);
+
     lastAssigned++;
     if (lastAssigned >= externalFirstAvailable) {
         // We have used up all of the sequence numbers that we have
