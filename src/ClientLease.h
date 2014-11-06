b3fbdb17aeb524b6967b0d3a63e0cd0f040c6528
Collin Lee <cstlee@cs.stanford.edu>
2014-11-05 18:40:53 -0800

    Add ClientLease implementation and tests.
    ClientLease is used by the client library to abstract the process
    of aquiring and maintaining a valid lease with the coordinator.
    These leases are used to represent the lifetime of an active client
    so that per client state stored on servers (e.g. linearizability
    data) can be garbage collected when clients fail or become inactive.

---
+++ src/ClientLease.h
@@ -0,0 +1,76 @@
+/* Copyright (c) 2014 Stanford University
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR(S) DISCLAIM ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL AUTHORS BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifndef RAMCLOUD_CLIENTLEASE_H
+#define RAMCLOUD_CLIENTLEASE_H
+
+#include "Common.h"
+#include "CoordinatorClient.h"
+#include "WireFormat.h"
+
+namespace RAMCloud {
+
+/**
+ * This class allows client rpcs to aquire and maintain valid leases.  These
+ * leases are used to represent the lifetime of an active client so that per
+ * client state stored on servers (e.g. linearizability data) can be garbage
+ * collected when clients fail or become inactive.
+ */
+class ClientLease {
+  public:
+    explicit ClientLease(Context* context);
+    WireFormat::ClientLease getLease();
+    void poll();
+
+  PRIVATE:
+    /// Shared information about the server.
+    Context* context;
+    /// Latest ClientLease received from the coordinator.
+    WireFormat::ClientLease lease;
+
+    /// The local time (in cycles) when the last ClientLease renewal was sent to
+    /// the coordinator.  Used to estimate when the lease term will elapse.
+    uint64_t localTimestampCycles;
+
+    /// Used the store the next value of localTimestampCycles so that it can
+    /// represent sent time with out modifying the value before the new lease
+    /// is received.
+    uint64_t nextTimestampCycles;
+
+    /// Holds a possibly outstanding RenewLeaseRpc so that this module can
+    /// use asynchronous calls to the coordinator to maintain its lease.
+    Tub<RenewLeaseRpc> renewLeaseRpc;
+
+    uint64_t leaseTermRemaining();
+
+    /// Defines the remaining lease time below which this module should starting
+    /// trying to renew.  During this period, the lease has probably not expired
+    /// so it is safe to perform the renewals asynchronously.  This value should
+    /// be set conservatively to around half the LeaseManager::LEASE_TERM_US.
+    static const uint64_t RENEW_THRESHOLD_US = 150*1e6;     // 2.5 min
+
+    /// Defines the remaining lease time below which this module should consider
+    /// the lease possibly expired.  In this case, the module must not return
+    /// the current lease until it as renewed.  This value should be set to a
+    /// value much larger than the time to complete a renewal.
+    static const uint64_t DANGER_THRESHOLD_US = 500;
+
+    DISALLOW_COPY_AND_ASSIGN(ClientLease);
+};
+
+} // namespace RAMCloud
+
+#endif  /* RAMCLOUD_CLIENTLEASE_H */
+
---
+++ src/ClientLeaseTest.cc
@@ -0,0 +1,151 @@
+/* Copyright (c) 2014 Stanford University
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR(S) DISCLAIM ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL AUTHORS BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include "TestUtil.h"       //Has to be first, compiler complains
+#include "MockCluster.h"
+#include "ClientLease.h"
+
+
+namespace RAMCloud {
+
+class ClientLeaseTest : public ::testing::Test {
+  public:
+    TestLog::Enable logEnabler;
+    Context context;
+    MockCluster cluster;
+    ClientLease lease;
+    uint64_t RENEW_THRESHOLD_US;
+    uint64_t DANGER_THRESHOLD_US;
+
+    ClientLeaseTest()
+        : logEnabler()
+        , context()
+        , cluster(&context)
+        , lease(&context)
+        , RENEW_THRESHOLD_US(ClientLease::RENEW_THRESHOLD_US)
+        , DANGER_THRESHOLD_US(ClientLease::DANGER_THRESHOLD_US)
+    {
+        // Silence the log messages as there are some standard warnings that
+        // the coordinator will spit out.
+        Logger::get().setLogLevels(RAMCloud::SILENT_LOG_LEVEL);
+    }
+
+    ~ClientLeaseTest()
+    {
+        // Set the the logger so that we don't affect later running tests.
+        Logger::get().reset();
+        Cycles::mockTscValue = 0;
+    }
+
+    DISALLOW_COPY_AND_ASSIGN(ClientLeaseTest);
+};
+
+TEST_F(ClientLeaseTest, getLease_basic) {
+    lease.localTimestampCycles = 0;
+    Cycles::mockTscValue = Cycles::fromNanoseconds(5000);
+    WireFormat::ClientLease l = {0, 0, 0};
+    lease.lease = l;
+    EXPECT_EQ(0U, l.leaseId);
+    EXPECT_EQ(0U, lease.lease.leaseId);
+    l = lease.getLease();
+    EXPECT_EQ(1U, l.leaseId);
+    EXPECT_EQ(1U, lease.lease.leaseId);
+}
+
+TEST_F(ClientLeaseTest, getLease_shouldAsyncRenew) {
+    lease.localTimestampCycles = 0;
+    uint64_t leaseTerm = 300*1e6;
+    uint64_t currentTimeUs = leaseTerm - RENEW_THRESHOLD_US + 1;
+    Cycles::mockTscValue = Cycles::fromNanoseconds(currentTimeUs * 1000);
+    WireFormat::ClientLease l = {0, leaseTerm, 0};
+    lease.lease = l;
+    EXPECT_EQ(0U, l.leaseId);
+    EXPECT_EQ(0U, lease.lease.leaseId);
+    EXPECT_FALSE(lease.renewLeaseRpc);
+    EXPECT_GE(lease.leaseTermRemaining(), DANGER_THRESHOLD_US);
+    l = lease.getLease();
+    EXPECT_EQ(0U, l.leaseId);
+    EXPECT_EQ(0U, lease.lease.leaseId);
+    EXPECT_TRUE(lease.renewLeaseRpc);
+}
+
+TEST_F(ClientLeaseTest, getLease_shouldSyncRenew) {
+    lease.localTimestampCycles = 0;
+    uint64_t leaseTerm = 300*1e6;
+    uint64_t currentTimeUs = leaseTerm - DANGER_THRESHOLD_US + 1;
+    Cycles::mockTscValue = Cycles::fromNanoseconds(currentTimeUs * 1000);
+    WireFormat::ClientLease l = {0, leaseTerm, 0};
+    lease.lease = l;
+    EXPECT_EQ(0U, l.leaseId);
+    EXPECT_EQ(0U, lease.lease.leaseId);
+    EXPECT_FALSE(lease.renewLeaseRpc);
+    EXPECT_LT(lease.leaseTermRemaining(), DANGER_THRESHOLD_US);
+    l = lease.getLease();
+    EXPECT_EQ(1U, l.leaseId);
+    EXPECT_EQ(1U, lease.lease.leaseId);
+    EXPECT_FALSE(lease.renewLeaseRpc);
+}
+
+TEST_F(ClientLeaseTest, poll) {
+    lease.localTimestampCycles = 0;
+    Cycles::mockTscValue = Cycles::fromNanoseconds(5000);
+    EXPECT_EQ(0U, lease.lease.leaseId);
+    EXPECT_FALSE(lease.renewLeaseRpc);
+    EXPECT_LT(lease.leaseTermRemaining(), RENEW_THRESHOLD_US);
+    lease.poll();
+    EXPECT_EQ(0U, lease.lease.leaseId);
+    EXPECT_TRUE(lease.renewLeaseRpc);
+    EXPECT_EQ(0U, lease.localTimestampCycles);
+    EXPECT_EQ(Cycles::fromNanoseconds(5000), lease.nextTimestampCycles);
+    Cycles::mockTscValue = Cycles::fromNanoseconds(15000);
+    lease.poll();
+    EXPECT_EQ(1U, lease.lease.leaseId);
+    EXPECT_FALSE(lease.renewLeaseRpc);
+    EXPECT_GE(lease.leaseTermRemaining(), RENEW_THRESHOLD_US);
+    EXPECT_EQ(Cycles::fromNanoseconds(5000), lease.localTimestampCycles);
+    EXPECT_EQ(Cycles::fromNanoseconds(5000), lease.nextTimestampCycles);
+    lease.poll();
+    EXPECT_EQ(1U, lease.lease.leaseId);
+    EXPECT_FALSE(lease.renewLeaseRpc);
+    EXPECT_GE(lease.leaseTermRemaining(), RENEW_THRESHOLD_US);
+    EXPECT_EQ(Cycles::fromNanoseconds(5000), lease.localTimestampCycles);
+    EXPECT_EQ(Cycles::fromNanoseconds(5000), lease.nextTimestampCycles);
+}
+
+TEST_F(ClientLeaseTest, leaseTermRemaining_basic) {
+    lease.lease.leaseTerm = 1000;
+    lease.lease.timestamp = 0;
+    lease.localTimestampCycles = 0;
+    Cycles::mockTscValue = Cycles::fromNanoseconds(10000);
+    EXPECT_EQ(990U, lease.leaseTermRemaining());
+}
+
+TEST_F(ClientLeaseTest, leaseTermRemaining_leaseTermInvalid) {
+    lease.lease.leaseTerm = 10;
+    lease.lease.timestamp = 1000;
+    lease.localTimestampCycles = 1;
+    Cycles::mockTscValue = 10;
+    EXPECT_EQ(0U, lease.leaseTermRemaining());
+}
+
+TEST_F(ClientLeaseTest, leaseTermRemaining_leaseTermElapsed) {
+    lease.lease.leaseTerm = 1000;
+    lease.lease.timestamp = 0;
+    lease.localTimestampCycles = 0;
+    Cycles::mockTscValue = Cycles::fromNanoseconds(1005000);
+    EXPECT_EQ(0U, lease.leaseTermRemaining());
+}
+
+} // namespace RAMCloud
