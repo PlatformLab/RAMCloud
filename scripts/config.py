PlatformLab/RAMCloud

commit 08936fcdb1fc045c4aad5d0294bf7edbb299224d

Changed the benchmarking scripts (ex: clusterperf) to by default use servers allocated by the user in rcres, a Stanford internal utility. The scripts will also validate that servers specified in localconfig (if any) are indeed reserved by the user at the time of invocation.
Persons without access to the internal tool can continue to use
their existing localconfig’s without modification.

Also, fixed a bug in common.py to kill the correct coordinator
after the e3978ca patch.

scripts/config.py
@@ -21,14 +21,15 @@ run RAMCloud scripts at your site.
 """
 
 from common import captureSh
+import commands
 import os
 import re
 import subprocess
 import sys
 
 __all__ = ['coordinator_port', 'default_disk1','default_disk2', 'git_branch',
-           'git_ref', 'git_diff', 'hosts', 'obj_dir', 'obj_path', 'scripts_path',
-           'second_backup_port', 'server_port', 'top_path']
+           'git_ref', 'git_diff', 'obj_dir', 'obj_path', 'scripts_path',
+           'second_backup_port', 'server_port', 'top_path', 'getHosts']
 
 # git_branch is the name of the current git branch, which is used
 # for purposes such as computing objDir.
@@ -81,16 +82,6 @@ if '/usr/local/lib' not in ld_library_path:
     ld_library_path.insert(0, '/usr/local/lib')
 os.environ['LD_LIBRARY_PATH'] = ':'.join(ld_library_path)
 
-# All of the hosts available for servers or clients; each entry
-# consists of a name for the host (for ssh), an IP address
-# to use for creating service locators. and an id for generating
-# Ethernet addresses.
-hosts = []
-for i in range(1, 61):
-    hosts.append(('rc%02d' % i,
-                  '192.168.1.%d' % (100 + i),
-                  i))
-
 # Host on which old master is run for running recoveries.
 # Need not be a member of hosts
 old_master_host = ('rcmaster', '192.168.1.1', 81)
@@ -114,6 +105,62 @@ default_disk2 = '-f /dev/sdb2'
 # Try to include local overrides.
 try:
     from localconfig import *
-except:
+except ImportError:
     pass
 
+
+# Returns a list of the hosts available for servers or clients;
+# each entry consists of a name for the host (for ssh), an IP address
+# to use for creating service locators, and an id for generating
+# Ethernet addresses.
+#
+# By default, the function will return a list generated from servers
+# locked by the current user in rcres (an RAMCloud internal utility).
+# If rcres is not available, a custom list can be defined in
+# localconfig.py (see below and the wiki for additional instructions).
+# In the event that rcres is available and a custom list is defined,
+# the function will validate the custom list against rcres.
+#
+# Example for constructing a custom list in localconfig.py:
+# hosts = []
+# for i in range(1, 61):
+#     hosts.append(('rc%02d' % i,
+#                   '192.168.1.%d' % (100 + i),
+#                   i))
+
+def getHosts():
+  # Find servers locked by user via rcres
+  rcresOutput = commands.getoutput('rcres ls -l | grep "$(whoami)" | cut -c13-16 | grep "rc[0-9]" | cut -c3-4')
+  rcresFailed = re.match(".*command not found.*", rcresOutput)
+
+  # If hosts overridden in localconfig.py, check that all servers are locked
+  if hosts:
+    requstedUnlockedHosts = []
+    for host in hosts:
+      if str("%02d" % host[2]) not in rcresOutput.split():
+        requstedUnlockedHosts.append(host[0])
+
+    if not rcresFailed and len(requstedUnlockedHosts) > 0:
+      raise Exception ("Manually defined hosts list in localconfig.py includes the "
+        "following servers not locked by user in rcres:\r\n\t%s" % requstedUnlockedHosts)
+
+    return hosts
+
+  # hosts has not been overridden, check that rcres has some servers for us
+  else:
+    if rcresFailed:
+      raise Exception ("config.py could not invoke rcres (%s);\r\n"
+        "\tplease specify a custom hosts list in scripts/localconfig.py" % rcresOutput)
+
+    if len(rcresOutput) == 0:
+      raise Exception ("config.py found 0 rcXX servers locked in rcres;\r\n"
+        "\tcheck your locks or specify a custom hosts list in scripts/localconfig.py")
+
+    # Everything checks out, build list
+    serverList = []
+    for hostNum in rcresOutput.split():
+      i = int(hostNum)
+      serverList.append(('rc%02d' % i,
+                         '192.168.1.%d' % (100 + i),
+                         i))
+    return serverList

scripts/cluster.py
@@ -196,7 +196,7 @@ class Cluster(object):
         self.masters_started = 0
         self.backups_started = 0
 
-        self.coordinator_host= hosts[0]
+        self.coordinator_host= getHosts()[0]
         self.coordinator_locator = coord_locator(self.transport,
                                                  self.coordinator_host)
         self.log_subdir = log.createDir(log_dir, log_exists)
@@ -319,14 +319,14 @@ class Cluster(object):
         """
         log_prefix = '%s/server%d.%s' % (
                       self.log_subdir, self.next_server_id, host[0])
-                     
+
         command = ('%s %s -C %s -L %s -r %d -l %s --clusterName __unnamed__ '
                    '--logFile %s.log --preferredIndex %d %s' %
                    (valgrind_command,
                     server_binary, self.coordinator_locator,
                     server_locator(self.transport, host, port),
                     self.replicas,
-                    self.log_level, 
+                    self.log_level,
                     log_prefix,
                     self.next_server_id,
                     args))
@@ -429,7 +429,7 @@ class Cluster(object):
             ensureCommand = ('%s -C %s -m %d -b %d -l 1 --wait %d '
                              '--logFile %s/ensureServers.log' %
                              (ensure_servers_bin, self.coordinator_locator,
-                             numMasters, numBackups, timeout, 
+                             numMasters, numBackups, timeout,
                              self.log_subdir))
             if self.verbose:
                 print("ensureServers command: %s" % ensureCommand)
@@ -578,7 +578,7 @@ def run(
         valgrind_args='',	   # Additional arguments for valgrind
         disjunct=False,            # Disjunct entities on a server
         coordinator_host=None
-        ):       
+        ):
     """
     Start a coordinator and servers, as indicated by the arguments.
     Then start one or more client processes and wait for them to complete.
@@ -591,24 +591,24 @@ def run(
 
     if verbose:
         print('num_servers=(%d), available hosts=(%d) defined in config.py'
-              % (num_servers, len(hosts)))
+              % (num_servers, len(getHosts())))
         print ('disjunct=', disjunct)
 
 # When disjunct=True, disjuncts Coordinator and Clients on Server nodes.
     if disjunct:
-        if num_servers + num_clients + 1 > len(hosts):
+        if num_servers + num_clients + 1 > len(getHosts()):
             raise Exception('num_servers (%d)+num_clients (%d)+1(coord) exceeds the available hosts (%d)'
-                            % (num_servers, num_clients, len(hosts)))
+                            % (num_servers, num_clients, len(getHosts())))
     else:
-        if num_servers > len(hosts):
+        if num_servers > len(getHosts()):
             raise Exception('num_servers (%d) exceeds the available hosts (%d)'
-                            % (num_servers, len(hosts)))
+                            % (num_servers, len(getHosts())))
 
     if not share_hosts and not client_hosts:
-        if (len(hosts) - num_servers) < 1:
+        if (len(getHosts()) - num_servers) < 1:
             raise Exception('Asked for %d servers without sharing hosts with %d '
                             'clients, but only %d hosts were available'
-                            % (num_servers, num_clients, len(hosts)))
+                            % (num_servers, num_clients, len(getHosts())))
 
     masters_started = 0
     backups_started = 0
@@ -626,7 +626,7 @@ def run(
         cluster.disk = disk1
         cluster.enable_logcabin = enable_logcabin
         cluster.disjunct = disjunct
-        cluster.hosts = hosts
+        cluster.hosts = getHosts()
 
         if not coordinator_host:
             coordinator_host = cluster.hosts[len(cluster.hosts)-1]

scripts/clusterperf.py
@@ -267,7 +267,7 @@ def indexBasic(name, options, cluster_args, client_args):
         cluster_args['timeout'] = 200
     # Ensure at least 5 hosts for optimal performance
     if options.num_servers == None:
-        cluster_args['num_servers'] = len(hosts)
+        cluster_args['num_servers'] = len(getHosts())
     cluster.run(client='%s/ClusterPerf %s %s' %
             (obj_path, flatten_args(client_args), name), **cluster_args)
     print(get_client_log(), end='')
@@ -287,7 +287,7 @@ def indexRange(name, options, cluster_args, client_args):
 
     # Ensure at least 5 hosts for optimal performance
     if options.num_servers == None:
-        cluster_args['num_servers'] = len(hosts)
+        cluster_args['num_servers'] = len(getHosts())
     cluster.run(client='%s/ClusterPerf %s %s' %
             (obj_path, flatten_args(client_args), name), **cluster_args)
     print(get_client_log(), end='')
@@ -299,19 +299,19 @@ def indexMultiple(name, options, cluster_args, client_args):
         cluster_args['timeout'] = 360
     # Ensure atleast 15 hosts for optimal performance
     if options.num_servers == None:
-        cluster_args['num_servers'] = len(hosts)
+        cluster_args['num_servers'] = len(getHosts())
 
     # use a maximum of 10 secondary keys
-    if len(hosts) <= 10:
+    if len(getHosts()) <= 10:
         # Hack until synchronization bug in write RPC handler
         # in MasterService is resolved. This bug prevents us from using more
         # than 1 MasterSerivice thread. However, we need to use more than 1
         # service thread, otherwise if a tablet and its corresponding
         # indexlet end up on the same server, we will have a deadlock.
         # For now, make sure that we never wrap around the server list
-        # Once the bug is resolved, we should be able to use len(hosts)
+        # Once the bug is resolved, we should be able to use len(getHosts())
         # for numIndexes
-        client_args['--numIndexes'] = len(hosts) - 1
+        client_args['--numIndexes'] = len(getHosts()) - 1
     else:
         client_args['--numIndexes'] = 10
 
@@ -335,7 +335,7 @@ def indexScalability(name, options, cluster_args, client_args):
 
     # Ensure at least 15 hosts for optimal performance
     if options.num_servers == None:
-        cluster_args['num_servers'] = len(hosts)
+        cluster_args['num_servers'] = len(getHosts())
     if 'num_clients' not in cluster_args:
         cluster_args['num_clients'] = 10
     cluster.run(client='%s/ClusterPerf %s %s' %
@@ -349,7 +349,7 @@ def indexWriteDist(name, options, cluster_args, client_args):
         cluster_args['timeout'] = 200
 
     if options.num_servers == None:
-        cluster_args['num_servers'] = len(hosts)
+        cluster_args['num_servers'] = len(getHosts())
 
     if '--count' not in client_args:
         client_args['--count'] = 10000
@@ -380,7 +380,7 @@ def indexReadDist(name, options, cluster_args, client_args):
         cluster_args['timeout'] = 200
 
     if options.num_servers == None:
-        cluster_args['num_servers'] = len(hosts)
+        cluster_args['num_servers'] = len(getHosts())
 
     if '--count' not in client_args:
         client_args['--count'] = 10000
@@ -434,7 +434,7 @@ def transactionThroughput(name, options, cluster_args, client_args):
     if cluster_args['timeout'] < 250:
         cluster_args['timeout'] = 250
     if 'num_clients' not in cluster_args:
-        cluster_args['num_clients'] = len(hosts) - cluster_args['num_servers']
+        cluster_args['num_clients'] = len(getHosts()) - cluster_args['num_servers']
     if cluster_args['num_clients'] < 2:
         print("Not enough machines in the cluster to run the '%s' benchmark"
                 % name)
@@ -452,7 +452,7 @@ def multiOp(name, options, cluster_args, client_args):
     if cluster_args['timeout'] < 100:
         cluster_args['timeout'] = 100
     if options.num_servers == None:
-        cluster_args['num_servers'] = len(hosts)
+        cluster_args['num_servers'] = len(getHosts())
     client_args['--numTables'] = cluster_args['num_servers'];
     cluster.run(client='%s/ClusterPerf %s %s' %
             (obj_path, flatten_args(client_args), name),
@@ -461,11 +461,11 @@ def multiOp(name, options, cluster_args, client_args):
 
 def netBandwidth(name, options, cluster_args, client_args):
     if 'num_clients' not in cluster_args:
-        cluster_args['num_clients'] = 2*len(config.hosts)
+        cluster_args['num_clients'] = 2*len(config.getHosts())
     if options.num_servers == None:
         cluster_args['num_servers'] = cluster_args['num_clients']
-        if cluster_args['num_servers'] > len(config.hosts):
-            cluster_args['num_servers'] = len(config.hosts)
+        if cluster_args['num_servers'] > len(config.getHosts()):
+            cluster_args['num_servers'] = len(config.getHosts())
     if options.size != None:
         client_args['--size'] = options.size
     else:
@@ -478,9 +478,9 @@ def readAllToAll(name, options, cluster_args, client_args):
     cluster_args['backups_per_server'] = 0
     cluster_args['replicas'] = 0
     if 'num_clients' not in cluster_args:
-        cluster_args['num_clients'] = len(hosts)
+        cluster_args['num_clients'] = len(getHosts())
     if options.num_servers == None:
-        cluster_args['num_servers'] = len(hosts)
+        cluster_args['num_servers'] = len(getHosts())
     client_args['--numTables'] = cluster_args['num_servers'];
     cluster.run(client='%s/ClusterPerf %s %s' %
             (obj_path, flatten_args(client_args), name), **cluster_args)
@@ -544,7 +544,7 @@ def readThroughput(name, options, cluster_args, client_args):
     if cluster_args['timeout'] < 250:
         cluster_args['timeout'] = 250
     if 'num_clients' not in cluster_args:
-        cluster_args['num_clients'] = len(hosts) - cluster_args['num_servers']
+        cluster_args['num_clients'] = len(getHosts()) - cluster_args['num_servers']
     if cluster_args['num_clients'] < 2:
         print("Not enough machines in the cluster to run the '%s' benchmark"
                 % name)
@@ -559,7 +559,7 @@ def txCollision(name, options, cluster_args, client_args):
     if cluster_args['timeout'] < 100:
         cluster_args['timeout'] = 100
     if options.num_servers == None:
-        cluster_args['num_servers'] = len(hosts)
+        cluster_args['num_servers'] = len(getHosts())
     #client_args['--numTables'] = cluster_args['num_servers'];
     if 'num_clients' not in cluster_args:
         cluster_args['num_clients'] = 5

scripts/common.py
@@ -162,7 +162,7 @@ class Sandbox(object):
                                                      p.sonce]))
 
             if self.cleanup:
-                chost = hosts[0] # coordinator
+                chost = getHosts()[-1] # coordinator
                 killers.append(subprocess.Popen(['ssh', chost[0],
                                     '%s/killcoord' % scripts_path]))
 

scripts/startserver.py
@@ -32,6 +32,7 @@ def start_server(options):
         else:
             hosts_used.append(file.split('_')[0])
 
+    hosts = getHosts()
     mhost = hosts[0] # just default value
     for host in hosts:
         # host[0] = rcxx

commit 1487710388f6409a842fbb49da780b9ca9d8c455

Fixed a bug introduced in 08936fc which causes the benchmarking scripts	to crash when a	user has a localconfig.py file but does not define the hosts variable.

scripts/config.py
@@ -134,7 +134,7 @@ def getHosts():
   rcresFailed = re.match(".*command not found.*", rcresOutput)
 
   # If hosts overridden in localconfig.py, check that all servers are locked
-  if hosts:
+  if 'hosts' in globals():
     requstedUnlockedHosts = []
     for host in hosts:
       if str("%02d" % host[2]) not in rcresOutput.split():