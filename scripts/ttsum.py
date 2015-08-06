87476a72473a35a8a46875cde15b0754f6ad03bf
John Ousterhout <ouster@cs.stanford.edu>
2015-08-06 15:04:36 -0700

    Changed ttsum.py output format.
    When using -f, it now prints min, max, etc. for cumulative time
    rather than delta.

--- scripts/ttsum.py
+++ scripts/ttsum.py
@@ -203,13 +203,13 @@ if options.startEvent:
             medianTime = times[len(times)//2]
             intervals.sort()
             message = '%-*s  %8.1f %8.1f %8.1f %8.1f %8.1f %7d' % (nameLength,
-                eventName, medianTime, intervals[len(intervals)//2],
-                intervals[0], intervals[-1], sum(intervals)/len(intervals),
-                len(intervals))
+                eventName, medianTime, times[0], times[-1],
+                sum(times)/len(times), intervals[len(intervals)//2],
+                len(times))
             outputInfo.append([medianTime, message])
 
     outputInfo.sort(key=lambda item: item[0])
-    print('%-*s      Time   Median      Min      Max  Average   Count' % (nameLength,
+    print('%-*s    Median      Min      Max  Average    Delta   Count' % (nameLength,
             "Event"))
     print('%s------------------------------------------------------' %
             ('-' * nameLength))
