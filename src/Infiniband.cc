04dc9ddafc5e45e43722985b1ffd04c04e68121d
John Ousterhout <ouster@cs.stanford.edu>
2015-08-19 14:15:04 -0700

    Improved support for multi-port Infiniband NICs.

--- src/Infiniband.cc
+++ src/Infiniband.cc
@@ -628,7 +628,7 @@ Infiniband::QueuePair::plumb(QueuePairTuple *qpt)
     qpa.ah_attr.dlid = qpt->getLid();
     qpa.ah_attr.sl = 0;
     qpa.ah_attr.src_path_bits = 0;
-    qpa.ah_attr.port_num = 1;
+    qpa.ah_attr.port_num = downCast<uint8_t>(ibPhysicalPort);
 
     r = ibv_modify_qp(qp, &qpa, IBV_QP_STATE |
                                 IBV_QP_AV |
