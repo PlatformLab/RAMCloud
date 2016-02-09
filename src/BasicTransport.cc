PlatformLab/RAMCloud

commit ec2bad7cc2543264b730ca400906fc0d1a0a0ee6

Incorrect iterator usage in BasicTransport
BasicTransport::SessioncancelRequest used an iterator after deleting
the element it referred to.
RAM-843 #close
RAM-842 #close

src/BasicTransport.cc
@@ -413,12 +413,15 @@ void
 BasicTransport::Session::cancelRequest(RpcNotifier* notifier)
 {
     for (ClientRpcMap::iterator it = t->outgoingRpcs.begin();
-            it != t->outgoingRpcs.end(); it++) {
+            it != t->outgoingRpcs.end(); ) {
         ClientRpc* clientRpc = it->second;
         if (clientRpc->notifier == notifier) {
-            uint64_t sequence = it->first;
-            t->outgoingRpcs.erase(sequence);
+            t->outgoingRpcs.erase(it);
             t->clientRpcPool.destroy(clientRpc);
+
+            // It's no longer safe to use "it", but at this point we're
+            // done (the RPC can't exist in the list twice).
+            return;
         }
     }
 }

commit 1fa5591252f8e47810504f061c0cb4e7ea53e719

Fixed potential infinite loop introduced by ec2bad7cc25

src/BasicTransport.cc
@@ -413,7 +413,7 @@ void
 BasicTransport::Session::cancelRequest(RpcNotifier* notifier)
 {
     for (ClientRpcMap::iterator it = t->outgoingRpcs.begin();
-            it != t->outgoingRpcs.end(); ) {
+            it != t->outgoingRpcs.end(); it++) {
         ClientRpc* clientRpc = it->second;
         if (clientRpc->notifier == notifier) {
             t->outgoingRpcs.erase(it);