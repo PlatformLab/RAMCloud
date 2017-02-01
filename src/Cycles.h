1538e32df52838fa9171b7896baf95c852021c0a
Henry Qin <hq6@stanford.edu>
2017-01-31 21:30:13 -0800

    Updating RAMCloud::Cycles to use rdtscp.
    rdtscp takes 12 ns instead of 7 ns for rdtsc on the rc cluster, but it ensures
    that all previous instructions have executed before it runs.

--- src/Cycles.h
+++ src/Cycles.h
@@ -31,7 +31,7 @@ class Cycles {
 
     /**
      * Return the current value of the fine-grain CPU cycle counter
-     * (accessed via the RDTSC instruction).
+     * (accessed via the RDTSCP instruction).
      */
     static __inline __attribute__((always_inline))
     uint64_t
@@ -42,7 +42,7 @@ class Cycles {
             return mockTscValue;
 #endif
         uint32_t lo, hi;
-        __asm__ __volatile__("rdtsc" : "=a" (lo), "=d" (hi));
+        __asm__ __volatile__("rdtscp" : "=a" (lo), "=d" (hi) : : "%rcx");
         return (((uint64_t)hi << 32) | lo);
     }
 
@@ -56,7 +56,7 @@ class Cycles {
     rdtsc_ignoreMockTsc()
     {
         uint32_t lo, hi;
-        __asm__ __volatile__("rdtsc" : "=a" (lo), "=d" (hi));
+        __asm__ __volatile__("rdtscp" : "=a" (lo), "=d" (hi) : : "%rcx");
         return (((uint64_t)hi << 32) | lo);
     }
 #endif
